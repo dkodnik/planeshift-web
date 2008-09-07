ae85af630d003bf6682eb183157ace72b834bd33
Khakilord <Khakilord@2752fbe2-5038-0410-9d0a-88578062bcef>
2008-09-07 00:33:55 +0000

    Removed the first wildcard from queries which were used on big tables that were frequently modified by psserver and npcclient.

--- www/webconsole-trunk/npcs/searchnpc.php
+++ www/webconsole-trunk/npcs/searchnpc.php
@@ -26,6 +26,9 @@ function searchnpc(){
   } else if ($name!=null and $name!="") {
     if (strstr($name,"*")) {
       $name = str_replace("*","%",$name);
+      if ($name[0] == "%") {
+	  $name = substr($name, 1);
+      }
       $query = "select id from characters where name like '$name'";
     } else {
       $query = "select id from characters where name='$name'";
