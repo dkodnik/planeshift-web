2f3df8a3df0d11cb06c954e8e33d96e0d3c3537b
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-01-22 22:41:50 +0000

    Some tests on statistics

--- www/webconsole/other/list_stats.php
+++ www/webconsole/other/list_stats.php
@@ -12,14 +12,14 @@ if ($groupid==1) {
 		// get max period name in this group, and create next
 		$nextquarter = getNextQuarterPeriod($groupid);
 		
-		$sql = "insert into statistics (id,groupid,periodname) values ('',".$groupid.",'".$nextquarter."')";
+		$sql = "insert into wc_statistics (groupid,periodname) values (".$groupid.",'".$nextquarter."')";
 		//echo "<h1>".$sql."</h1>";
 		$query = mysql_query2($sql);
 	}
 
 	$statquery = "select count(*) from accounts where created_date>=DATE('param1') and created_date<DATE('param2');";
 
-	$sql = "select * from statistics where groupid = ".$groupid." order by periodname";
+	$sql = "select * from wc_statistics where groupid = ".$groupid." order by periodname";
 	$query = mysql_query2($sql);
 	echo'<table border="1">';
 	$i = 0;
@@ -44,7 +44,7 @@ if ($groupid==1) {
     for ($i = 0; $i < sizeof($array); $i++){
 		$element = $arrayres[$i];
 		if ($element !=null && $element!="") {
-			$scaledvalue = $element / 200;
+			$scaledvalue = $element / 1;
 			echo "<td valign=bottom align=center><img src=images/bluebar2.gif height=".$scaledvalue." width=20></td>";
 		} else {
 			echo "<td><a href=\"index.php?page=list_stat_group&groupid=".$groupid;
@@ -65,5 +65,31 @@ if ($groupid==1) {
 	echo "&op=add\">Add next period</A><br><br>";
 }
 	
-}	
+}
+
+function getNextQuarterPeriod($groupid) {
+
+	$sql = "select max(periodname) as max from wc_statistics where groupid = ".$groupid." order by periodname";
+
+    $query = mysql_query2($sql);
+	echo'<table border="1">';
+	$i = 0;
+	while ($result = mysql_fetch_array($query)){
+		$max = $result['max'];
+    }
+    
+    $year = substr($max,0,4);
+    $quarter = substr($max,5,6);
+    
+    echo $quarter . " " . $year;
+    
+    if ($quarter=="Q4") {
+      $year = $year+1;
+      $quarter = "Q1";
+    } else
+      $quarter = "Q". (substr($quarter,1,2) + 1);
+
+    return $year." ". $quarter;
+}
+	
 ?>
