2bcc1e298dd67c9ad675b5aa3825b4a575d6588c
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-07-05 12:27:55 +0000

    Improved patch for guild listing and added edit/delete function for each member in the guild patch by nakem

--- webconsole-new/other/guilds.php
+++ webconsole-new/other/guilds.php
@@ -2,8 +2,8 @@
 function listguilds() {
     if(CheckAccess('other', 'read'))
     {
-        $guild = intval(@$_GET['guild']);
-        if(empty($guild)) // If the user hasn't selected a guild we'll just present a list.
+        $guild = @$_GET['guild'];
+        if(!is_numeric($guild)) // If the user hasn't selected a guild we'll just present a list.
         {
             echo '<p class="header">Guild List</p>';
             
@@ -37,7 +37,7 @@ function listguilds() {
         }
         else // Now we can get into the details.
         {
-            $sql = 'SELECT g.*, c.name AS founder_name, a.name AS alliance_name, g2.name AS leading_guild from guilds AS g LEFT JOIN alliances AS a ON a.id = g.alliance LEFT JOIN guilds AS g2 ON g2.id = a.leading_guild LEFT JOIN characters AS c ON c.id=g.char_id_founder WHERE g.id='.$guild;
+            $sql = 'SELECT g.*, c.name AS founder_name, a.name AS alliance_name, g2.name AS leading_guild from guilds AS g LEFT JOIN alliances AS a ON a.id = g.alliance LEFT JOIN guilds AS g2 ON g2.id = a.leading_guild LEFT JOIN characters AS c ON c.id=g.char_id_founder WHERE g.id='.$guild.' LIMIT 1';
             $row = mysql_fetch_array(mysql_query2($sql), MYSQL_ASSOC);
             
             echo '<p class="header">Guild Infos for "'.htmlentities($row['name']).'"</p>';
@@ -113,17 +113,139 @@ function listguilds() {
                 echo '<td>'.htmlentities($row['name']).'</td>';
                 echo '<td>'.htmlentities($row['guild_level']).'</td>';
                 echo '<td>'.htmlentities($row['guild_points']).'</td>';
-                echo '<td>';
-                /* Just waiting for the account page to come.
-                 * 
-                echo '<a href="./index.php?do=editaccounts&id='.$row['id'].'">Edit</a>';
+                echo '<td><a href="./index.php?do=editguildmember&id='.$row['id'].'">Edit</a>';
                 if(CheckAccess('other', 'delete'))
                 {
-                    echo ' - <a href="./index.php?do=deleteaccounts&id='.$row['id'].'">Delete</a>';
-                }*/
+                    echo ' - <a href="./index.php?do=deleteguildmember&id='.$row['id'].'&guild='.$guild.'">Delete</a>';
+                }
                 echo '</td></tr>';
             }
             echo '</table>';
+        }
+    }
+    else
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+    }
+}
+
+function editguildmember()
+{
+    if(!CheckAccess('other', 'read'))
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+        return;
+    }
+    $edit = CheckAccess('other', 'edit');
+    $member = @$_GET['id'];
+    if(!is_numeric($member))
+    {
+        echo '<p class="error">You have to specify a valid member ID!</p>';
+        return;
+    }
+    
+    if(isset($_POST['guild_public_notes']) && isset($_POST['guild_private_notes']))
+    {
+        $public_notes = mysql_real_escape_string(str_replace("\r", '', $_POST['guild_public_notes']));
+        $private_notes = mysql_real_escape_string(str_replace("\r", '', $_POST['guild_private_notes']));
+        
+        $sql = 'UPDATE characters SET guild_public_notes = \''.$public_notes.'\', guild_private_notes = \''.$private_notes.'\' WHERE id = '.$member.' LIMIT 1';
+        $query = mysql_query2($sql);
+        if(1 == ($rows = mysql_affected_rows()))
+        {
+            echo '<p class="error">Update successful.</p>';
+        }
+        else
+        {
+            echo '<p class="error">Something went wrong while updating! (updated rows '.$rows.')</p>';
+        }
+    }
+    
+    echo '<p class="header">'.($edit ? 'Edit' : 'View').' a character\'s guild notes</p>';
+    
+    $sql = 'SELECT c.*, g.name as guild FROM characters AS c, guilds AS g WHERE c.id = '.$member.' AND g.id = c.guild_member_of LIMIT 1';
+    $row = mysql_fetch_array(mysql_query2($sql), MYSQL_ASSOC);
+    
+    echo '<a href="./index.php?do=listguilds&guild='.$row['guild_member_of'].'">Back</a><br/>';
+    if($edit)
+    {
+        echo '<form action="./index.php?do=editguildmember&id='.$member.'" method="post">';
+    }
+    echo '<table>';
+    echo '<tr class="color_a"><td>ID</td><td>'.$member.'</td></tr>';
+    echo '<tr class="color_b"><td>Name</td><td>'.htmlentities($row['name']).'</td></tr>';
+    echo '<tr class="color_a"><td>Guild</td><td>'.htmlentities($row['guild']).'</td></tr>';
+    echo '<tr class="color_b"><td>Guild Public Notes</td><td>';
+    echo ($edit ? '<textarea name="guild_public_notes" style="width: 400px; height: 100px;">' : '<div style="width: 400px; height: 100px;overflow: auto;"><pre style="margin: 0px;white-space: normal;">');
+    $notes = htmlentities($row['guild_public_notes']);
+    echo ($edit ? $notes : nl2br($notes));
+    echo ($edit ? '</textarea>' : '</pre></div>').'</td></tr>';
+    
+    echo '<tr class="color_a"><td>Guild Private Notes</td><td>';
+    echo ($edit ? '<textarea name="guild_private_notes" style="width: 400px; height: 100px;">' : '<div style="width: 400px; height: 100px;overflow: auto;"><pre style="margin: 0px;white-space: normal;">');
+    $notes = htmlentities($row['guild_private_notes']);
+    echo ($edit ? $notes : nl2br($notes));
+    echo ($edit ? '</textarea>' : '</pre></div>').'</td></tr>';
+    echo '</table>';
+    
+    if($edit)
+    {
+        echo '<input type="submit" value="update" /></form>';
+    }
+}
+
+function deleteguildmember()
+{
+    if(CheckAccess('other', 'delete'))
+    {
+        $id = @$_GET['id'];
+        $guild = @$_GET['guild'];
+        if(!is_numeric($id))
+        {
+            echo '<p class="error">You have to specify a valid member ID!</p>';
+            return;
+        }
+        if(!is_numeric($guild))
+        {
+            echo '<p class="error">You have to specify a valid guild ID!</p>';
+            return;
+        }
+        
+        $sql = 'SELECT * FROM characters WHERE id = '.$id.' LIMIT 1';
+        $row = mysql_fetch_array(mysql_query2($sql), MYSQL_ASSOC);
+        
+        $passed = false;
+        $password = @$_POST['password'];
+        if(!empty($password))
+        {
+            $sql = 'SELECT username FROM accounts WHERE password = \''.md5($password).'\' LIMIT 1';
+            $row2 = mysql_fetch_array(mysql_query2($sql), MYSQL_ASSOC);
+            if($_SESSION['username'] == $row2['username'])
+            {
+                $passed = true;
+            } 
+        }
+        if(!$passed)
+        {
+            echo '<strong>Are sure you want to remove "'.$row['name'].'" from the guild?</strong><br/>';
+            echo '<form action="./index.php?do=deleteguildmember&id='.$id.'&guild='.$guild.'" method="post">';
+            echo 'Password: <input type="password" name="password" /><br/>';
+            echo '<input type="submit" value="Yes"/>';
+            echo ' - <a href="./index.php?do=listguilds&guild='.$guild.'">No</a></form>';
+        }
+        else
+        {
+            $sql = 'UPDATE characters SET guild_member_of = 0 WHERE id = '.$id.' LIMIT 1';
+            $query = mysql_query2($sql);
+            if(1 == ($rows = mysql_affected_rows()))
+            {
+                echo '<p class="error">"'.htmlentities($row['name']).'" now belongs to no guild.</p>';
+            }
+            else
+            {
+                echo '<p class="error">Something went wrong! '.$rows.' characters are excluded from their guild.</p>';
+            }
+            echo '<a href="./index.php?do=listguilds&guild='.$guild.'">Back</a>';
         }
     }
     else
