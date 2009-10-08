ba6305c86700b818beb335fe5e6472a55ef1b5e3
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2009-10-08 19:07:53 +0000

    ported "list statistics" from old wc. patch by nakem

--- www/webconsole-new/other/othermain.php
+++ www/webconsole-new/other/othermain.php
@@ -7,7 +7,8 @@ function othermain()
         echo '<a href="./index.php?do=listguilds">List guilds and members</a> <br/>';
         echo '<a href="./index.php?do=listpetitions">List petitions</a> <br/>';
         echo '<a href="./index.php?do=listaccounts">List accounts</a> <br/>';
-        echo '<a href="./index.php?do=listcharacters">List characters</a <br/>';
+        echo '<a href="./index.php?do=listcharacters">List characters</a> <br/>';
+        echo '<a href="./index.php?do=liststats&groupid=1">List Stats</a> <br/>';
         /*
          * These pages still need to be done!
          *
