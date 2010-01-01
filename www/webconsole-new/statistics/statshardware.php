ee8fb8d5113552a5ff764efda839d28b23a68c46
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-01-01 16:10:20 +0000

    Improved readability, order, added threshold filter

--- www/webconsole-new/statistics/statshardware.php
+++ www/webconsole-new/statistics/statshardware.php
@@ -2,16 +2,26 @@
 
 function statshardware()
 {
-    if(checkaccess('statistics', 'read'))
+	//TODO FIXME 
+    //if(checkaccess('statistics', 'read'))
+	if (true)
     {
-        echo '<p class="header">Operating Systems</p>';
+		?>
+		<form action="./index.php?do=statshardware" METHOD="POST"><input type=text value=10 name=filter size=4> Threshold for filtering results <INPUT TYPE="SUBMIT" NAME="Filter" VALUE="Filter"></form>
+        <?php
+		echo '<p class="header">Operating Systems</p>';
 
-		$sql = "SELECT operating_system,count(operating_system) as result FROM accounts group by operating_system";
+		$filter = 0;
+		if ( isset($_POST['filter']) ) {
+			$filter = $_POST['filter'];
+		}
+
+		$sql = "SELECT operating_system,count(operating_system) as result FROM accounts where operating_system <>'' group by operating_system having result>=".$filter." order by result desc";
 		$query = mysql_query2($sql);
 
 		if(mysql_num_rows($query) < 1)
 		{
-			echo '<p class="error">No data found!</p>';
+			echo '<p class="error">No data found! Try lowering the threshold</p>';
 		}
         $line2 = '';
         $line3 = '';
@@ -19,7 +29,7 @@ function statshardware()
         while($result = mysql_fetch_array($query, MYSQL_ASSOC))
         {
             echo '<th>'.htmlentities($result['operating_system']).'</th>';
-            $line2 .= '<td>';
+            $line2 .= '<td valign=bottom>';
             $line2 .= '<img src="img/bluebar2.gif" width="20" height="'.($result['result'] / 1).'" />';
             $line2 .= '</td>';
             $line3 .= '<td>'.(is_numeric($result['result']) ? $result['result'] : '').'</td>';
@@ -29,12 +39,12 @@ function statshardware()
 
         echo '<p class="header">Graphics Cards</p>';
 
-		$sql = "SELECT graphics_card,count(graphics_card) as result FROM accounts group by graphics_card";
+		$sql = "SELECT graphics_card,count(graphics_card) as result FROM accounts where graphics_card <>'' group by graphics_card having result>=".$filter." order by result desc";
 		$query2 = mysql_query2($sql);
 
 		if(mysql_num_rows($query2) < 1)
 		{
-			echo '<p class="error">No data found!</p>';
+			echo '<p class="error">No data found! Try lowering the threshold</p>';
 		}
         $line2 = '';
         $line3 = '';
@@ -42,7 +52,7 @@ function statshardware()
         while($result2 = mysql_fetch_array($query2, MYSQL_ASSOC))
         {
             echo '<th>'.htmlentities($result2['graphics_card']).'</th>';
-            $line2 .= '<td>';
+            $line2 .= '<td valign=bottom>';
             $line2 .= '<img src="img/bluebar2.gif" width="20" height="'.($result2['result'] / 1).'" />';
             $line2 .= '</td>';
             $line3 .= '<td>'.(is_numeric($result2['result']) ? $result2['result'] : '').'</td>';
@@ -52,12 +62,12 @@ function statshardware()
 
         echo '<p class="header">Graphics Version</p>';
 
-		$sql = "SELECT graphics_version,count(graphics_version) as result FROM accounts group by graphics_version";
+		$sql = "SELECT graphics_version,count(graphics_version) as result FROM accounts where graphics_version <>'' group by graphics_version having result>=".$filter." order by result desc";
 		$query2 = mysql_query2($sql);
 
 		if(mysql_num_rows($query2) < 1)
 		{
-			echo '<p class="error">No data found!</p>';
+			echo '<p class="error">No data found! Try lowering the threshold</p>';
 		}
         $line2 = '';
         $line3 = '';
@@ -65,7 +75,7 @@ function statshardware()
         while($result2 = mysql_fetch_array($query2, MYSQL_ASSOC))
         {
             echo '<th>'.htmlentities($result2['graphics_version']).'</th>';
-            $line2 .= '<td>';
+            $line2 .= '<td valign=bottom>';
             $line2 .= '<img src="img/bluebar2.gif" width="20" height="'.($result2['result'] / 1).'" />';
             $line2 .= '</td>';
             $line3 .= '<td>'.(is_numeric($result2['result']) ? $result2['result'] : '').'</td>';
