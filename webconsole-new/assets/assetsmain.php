8a8ab82d8812ce23bb382d5a58feb1008d95ed20
lpancallo <lpancallo@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-01-12 22:10:43 +0000

    First implementation of assets-checker

---
+++ webconsole-new/assets/assetsmain.php
@@ -0,0 +1,18 @@
+<?php
+function assetsmain()
+{
+    if (checkaccess('assets', 'read'))
+    {
+        echo '<div class="menu_left">';
+        echo '<a href="./index.php?do=assetsnpc">NPCs</a> <br/>';
+
+        echo '<hr />';
+        echo '<a href="./index.php">Return to main page</a>';
+        echo '</div><div class="main">'."\n";
+    }
+    else
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+    }
+}
+?>
