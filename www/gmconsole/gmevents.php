b4bb8bae3b31715996e5577414da26c059c87d2c
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-01-21 23:19:41 +0000

    some minor fixes
    Patch by Drey

--- www/gmconsole/gmevents.php
+++ www/gmconsole/gmevents.php
@@ -8,15 +8,15 @@ session_start();
 
 // is the user logged in?
 if (!isset($_SESSION["__SECURITY_LEVEL"])) {
-	header("Location: index.php?origin=gmevents");
-	exit;
+    header("Location: index.php?origin=gmevents");
+    exit;
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
-	<head>
-		<title>GM Events</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <head>
+        <title>GM Events</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" href="<?=$__CSS_RELURI?>"/>
         <link rel="stylesheet" type="text/css" href="<?=$__CSS_ADDON_RELURI?>"/>
         <script language="javascript" type="text/javascript">
@@ -26,44 +26,46 @@ if (!isset($_SESSION["__SECURITY_LEVEL"])) {
                 document.getElementById("eventListForm").submit();
             }
         </script>
-	</head>
+    </head>
     <body>
-		<table>
-			<tr>
-				<td style="vertical-align:top;">
+        <table>
+            <tr>
+                <td style="vertical-align:top;">
 <?php
-					echo Navigation::S_GetNavigation();
+                    echo Navigation::S_GetNavigation();
 ?>
-				</td>
-				<form id="eventListForm" action="gmeventdetails.php" method="post">
-				<td style="width:800px; vertical-align:top;">
-					<table class="table">
-						<tr>
-							<th>Name</th>
-							<th>GM</th>
-							<th>Vote Average</th>
-							<th>Action</th>
-						</tr>
-						
-						<?php 
-							$GM_events = new PSGmEvents();
-							$events = $GM_events->getEvents();
-							foreach ($events as $event) {
-								echo '<tr>';
-								echo '<td>' . $event['name'] . '</td>';
-								echo '<td>' . $event['gm_name'] . ' ' . $event['gm_lastname'] . '</td>';
-								echo '<td>' . $event['avg'] . '</td>';
-								echo '<td><a href="javascript:setEventId(\'' . $event['id'] . '\');">View</a></td>';
-								echo '</tr>';
-							}
-							
-						?>
-					</table>
+                </td>
+                <form id="eventListForm" action="gmeventdetails.php" method="post">
+                <td style="width:800px; vertical-align:top;">
+                    <table class="table">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>GM</th>
+                            <th>Vote Average</th>
+                            <th>Action</th>
+                        </tr>
+                        
+                        <?php 
+                            $GM_events = new PSGmEvents();
+                            $events = $GM_events->getEvents();
+                            foreach ($events as $event) {
+                                echo '<tr>';
+                                echo '<td>' . $event['id'] . '</td>';
+                                echo '<td>' . $event['name'] . '</td>';
+                                echo '<td>' . $event['gm_name'] . ' ' . $event['gm_lastname'] . '</td>';
+                                echo '<td>' . $event['avg'] . '</td>';
+                                echo '<td><a href="javascript:setEventId(\'' . $event['id'] . '\');">View</a></td>';
+                                echo '</tr>';
+                            }
+                            
+                        ?>
+                    </table>
                         <input type="hidden" id="eventId" name="eventId"/>
                 </td>
             </tr>
         </table>
     </body>
                         
-	
+    
 </html>
