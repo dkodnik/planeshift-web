fb2019da9c6b29edcbdb62f451e49f1d8b6e9d28
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-01-31 17:30:47 +0000

    Removed an unused ID and made compatible with E_STRICT + coding standards.

--- www/webconsole-new/npcs/listnpcsector.php
+++ www/webconsole-new/npcs/listnpcsector.php
@@ -1,29 +1,33 @@
 <?php
-function listnpcsector(){
-  if (!checkaccess('npcs', 'read')){
-    echo '<p class="error">You are not authorized to use these functions</p>';
-	return;
-  }
+function listnpcsector()
+{
+    if (!checkaccess('npcs', 'read'))
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+        return;
+    }
   
-  echo '<h1>List of invulnerable NPCs divided by sector</h1>';
-  echo '<table border="1">';
-  echo '<th>Count</th><th>Sector</th><th>Race</th>';
-    $query = "select count(r.name) as num, s.name as sector, r.name as race from characters c, race_info r, sectors s where c.racegender_id=r.id and s.id=c.loc_sector_id and c.npc_impervious_ind='N' and c.character_type=1 group by s.name,r.name order by s.name";
-
+    echo '<h1>List of invulnerable NPCs divided by sector</h1>';
+    $query = "SELECT count(r.name) AS num, s.name AS sector, r.name AS race FROM characters c, race_info r, sectors s WHERE c.racegender_id=r.id AND s.id=c.loc_sector_id AND c.npc_impervious_ind='N' AND c.character_type='1' GROUP BY s.name,r.name ORDER BY s.name";    
     $result = mysql_query2($query);
-    if (mysql_num_rows($result) > 0){
-      while ($row = mysql_fetch_array($result)){
-        echo '<tr>';
-        echo '<td>'.$row['num'].'</td>';
-        $T_id = $row['id'];
-        echo '<td>'.$row['sector'].'</td>';
-        echo '<td>'.$row['race'].'</td>';
-        echo '</tr>';
-      }
-      echo '</table>';
-    }else{
-      echo '</table>';
-      echo '<p class="error">No NPCs Found</p>';
+    if (mysql_num_rows($result) > 0)
+    {
+        echo '<table border="1">';
+        echo '<th>Count</th><th>Sector</th><th>Race</th>';
+        while ($row = mysql_fetch_array($result))
+        {
+            echo '<tr>';
+            echo '<td>'.$row['num'].'</td>';
+            echo '<td>'.$row['sector'].'</td>';
+            echo '<td>'.$row['race'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    }
+    else
+    {
+        echo '</table>';
+        echo '<p class="error">No NPCs Found</p>';
     }
 }
 ?>
