c7675c1e4e1f6be526a9c54733d8e623ff4f2847
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-02-05 16:14:45 +0000

    Fixed an old bug that caused the description of the quest to overwrite the category. Fixed a second bug that caused you to be unable to change the description while editing a quest. Made the formatting for edit and create quests a bit more decent.

--- webconsole-new/quests/createquest.php
+++ webconsole-new/quests/createquest.php
@@ -4,32 +4,31 @@ function createquest(){
     if(!isset($_GET['commit'])){
       echo '<p class="header">Creating a new Quest</p>';
       echo '<form action="index.php?do=createquest&amp;commit" method="post">';
-      echo '<p class="bold">Quest ID will be generated automatically<br/>';
-      echo 'Quest Name: <input type="text" name="name" size="30"/><br/>';
-      echo 'Category: <input type="text" name="category" size="30"/><br/>';
-      echo 'Player Lockout: <input type="text" name="player_lockout" size="30"/><br/>';
-      echo 'Quest Lockout: <input type="text" name="quest_lockout" size="30"/><br/>';
-      echo 'Quest Description: <textarea rows="2" cols="40" name="description"></textarea><br/>';
-      echo '<input type="submit" name="submit" value="Create Quest" /></p>';
+      echo '<p class="bold">Quest ID will be generated automatically';
+      echo '<table border="0"> <tr><td>Quest Name:</td><td> <input type="text" name="name" size="30"/></td></tr>';
+      echo '<tr><td>Category:</td><td> <input type="text" name="category" size="30"/></td></tr>';
+      echo '<tr><td>Player Lockout:</td><td> <input type="text" name="player_lockout" size="30"/></td></tr>';
+      echo '<tr><td>Quest Lockout:</td><td> <input type="text" name="quest_lockout" size="30"/></td></tr>';
+      echo '<tr><td>Quest Description:</td><td> <textarea rows="2" cols="40" name="description"></textarea></td></tr>';
+      echo '<tr><td><input type="submit" name="submit" value="Create Quest" /></td><td></td></tr></table></p>';
       echo '</form>';
     }else{
  //Here we create the quest
       $name = mysql_real_escape_string($_POST['name']);
       $player_lockout = mysql_real_escape_string($_POST['player_lockout']);
       $quest_lockout = mysql_real_escape_string($_POST['quest_lockout']);
+      $category = mysql_real_escape_string($_POST['category']);
       $description = mysql_real_escape_string($_POST['description']);
-      $category = mysql_real_escape_string($_POST['description']);
       $id = GetNextId('quests');
       $query = "INSERT INTO quests (id, name, task, player_lockout_time, quest_lockout_time, category) VALUES ('$id', '$name', '$description', '$player_lockout', '$quest_lockout', '$category')";
       $result = mysql_query2($query);
       $query = "INSERT INTO quest_scripts (quest_id, script) VALUES ('$id', '#New Quest - Please Update')";
       $result = mysql_query2($query);
-?>
-    <SCRIPT language="javascript">
-      document.location = "index.php?do=editquest&id=<?echo $id?>";
-    </script>
-<?php
-    exit;
+
+    echo '<SCRIPT language="javascript">';
+    echo 'document.location = "index.php?do=editquest&id='.$id.'";';
+    echo '</script>';
+
     }
   }else{
     echo '<p class="error">You are not authorized to use these functions</p>';
