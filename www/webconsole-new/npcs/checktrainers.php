f86d8d81def9a6dff2afc2e0e965fb151d63ec2e
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-09-22 17:00:17 +0000

    Fixed checktrainer. Gaps in ranges are now properly shown, colors have changed to reflect severity of the warning/error. Commented the code a bit, and combined the queries. This should help overall readability.

--- www/webconsole-new/npcs/checktrainers.php
+++ www/webconsole-new/npcs/checktrainers.php
@@ -1,39 +1,48 @@
 <?php
 
-function sum_ranges($current, $new, $npc, $names, $sectors) {
+/**
+*  Notice that on any SVN database, this page will likely not find any trainers, since all NPCrooms are explicitly excluded from the query.
+*/
+
+// This is a support method for checktrainers().
+function sum_ranges($current, $new) { 
     $finished = false;
     foreach($current as $i => $range)
     {
+        // It's not in this range, if, at the end of this loop, it did not match any ranges, it will be added as a new one.
         if($range['max'] < $new['min'] || $range['min'] > $new['max'])
-        { // It's not in this range...
+        { 
             continue;
         }
-        if($range['max'] >= $new['max'] && $range['min'] <= $new['min'])
-        {// Included in this range.
-            $range['status'] .= '<a href="./index.php?do=npc_details&sub=main&npc_id='.$npc.'"><span style="color: yellow;">'.htmlentities($names[$npc]).' ('.$sectors[$npc].'): Range '.$new['min'].'-'.$new['max'].' is included in '.$range['min'].'-'.$range['max'].'!</span></a><br/>';
+        if($range['max'] >= $new['max'] && $range['min'] <= $new['min']) // Included in this range.
+        {
+            $range['status'] .= '<a href="./index.php?do=npc_details&sub=main&npc_id='.$new['player_id'].'"><span style="color: yellow;">'.htmlentities($new['name']).' ('.$new['sector'].'): Range '.$new['min'].'-'.$new['max'].' is included in '.$range['min'].'-'.$range['max'].'!</span></a><br/>';
             $current[$i] = $range;
             $finished = true;
             break;
         }
-        if($range['max'] == $new['min'])
-        {// Extends this range.
+        // Note that we do not need to check if we need to glue multiple ranges, because the data arrives sorted by min rank.
+        if($range['max'] == $new['min']) // Extends this range.
+        {
             if($range['max'] < $new['max'])
             {
                 $range['max'] = $new['max'];
             }
-            $range['npc'][] = $npc;
+            $range['npc'][] = $new['name'];
             $current[$i] = $range;
             $finished = true;
             break;
         }
         else
         {
+            // overlapping entry.
             $npcs = $range['npc'];
-            foreach($npcs as $z => $id)
+            if($range['max'] < $new['max'])
             {
-                $npcs[$z] = $names[$id];
+                $range['max'] = $new['max'];
             }
-            $range['status'] .= '<a href="./index.php?do=npc_details&sub=main&npc_id='.$npc.'"><span style="color: red;">'.htmlentities($names[$npc]).' ('.$sectors[$npc].'): Range '.$new['min'].'-'.$new['max'].' is overlapping with '.$range['min'].'-'.$range['max'].'('.implode(', ', $npcs).')!</span></a><br/>';
+            $range['npc'][] = $new['name'];
+            $range['status'] .= '<a href="./index.php?do=npc_details&sub=main&npc_id='.$new['player_id'].'"><span style="color: yellow;">'.htmlentities($new['name']).' ('.$new['sector'].'): Range '.$new['min'].'-'.$new['max'].' is overlapping with '.$range['min'].'-'.$range['max'].'('.implode(', ', $npcs).')!</span></a><br/>';
             $current[$i] = $range;
             $finished = true;
             break;
@@ -55,25 +64,18 @@ function checktrainers()
     {
         echo '<p class="header">Check Trainers</p>';
         
-        $sql = 'SELECT c.id, c.name, s.name AS sector_name FROM characters AS c JOIN sectors AS s ON c.loc_sector_id=s.id WHERE character_type=1 AND s.name!="NPCroom" AND s.name!="NPCroom1" AND s.name!="NPCroom2" AND s.name!="NPCroom3"';
-        $query = mysql_query2($sql);
-        $names = array();
-        $sectors = array();
-        while($row = mysql_fetch_array($query, MYSQL_ASSOC))
-        {
-            $names[$row['id']] = $row['name'];
-            $sectors[$row['id']] = $row['sector_name'];
-        }
-        
-        $sql = 'SELECT player_id, skill_id, min_rank, max_rank FROM trainer_skills ORDER BY min_rank';
+        $sql = 'SELECT ts.player_id, ts.skill_id, ts.min_rank, ts.max_rank, c.id, c.name, s.name AS sector_name FROM trainer_skills AS ts LEFT JOIN characters AS c ON ts.player_id=c.id LEFT JOIN sectors AS s ON c.loc_sector_id=s.id WHERE character_type=1  ORDER BY min_rank';//AND s.name NOT LIKE "NPCroom%"
         $query = mysql_query2($sql);
         $skills = array();
+        // $skills contains as first index an ID. Then on the next dimension, we find an array filled with ranges, each range is an array itself containing min, max, 
+        // array(npc), status, name, player_id, sector. The "array(npc) will later contain all npcs belonging to the combined range.
+        // $skills[ID][RANGE_NR][RANGE_ELEMENT] is thus a 3d array, with the exception of the element npc, at which point it is 4d.
         
         while($row = mysql_fetch_array($query, MYSQL_ASSOC))
         {
             if($row['max_rank'] < $row['min_rank'])
             {
-                $msg = '<a href="./index.php?do=npc_details&sub=main&npc_id='.$row['player_id'].'"><span style="color: red;">'.htmlentities($names[$row['player_id']]).': cannot define higher min ('.$row['min_rank'].') than max ('.$row['max_rank'].')!</span></a><br/>';
+                $msg = '<a href="./index.php?do=npc_details&sub=main&npc_id='.$row['player_id'].'"><span style="color: red;">'.htmlentities($row['name']).': cannot define higher min ('.$row['min_rank'].') than max ('.$row['max_rank'].')!</span></a><br/>';
                 if(isset($skills[$row['skill_id']]))
                 {
                     $skills[$row['skill_id']][0]['status'] .= $msg;
@@ -87,11 +89,11 @@ function checktrainers()
             }
             if(isset($skills[$row['skill_id']]))
             {
-                $skills[$row['skill_id']] = sum_ranges($skills[$row['skill_id']], array('min' => $row['min_rank'], 'max' => $row['max_rank']), $row['player_id'], $names, $sectors);
+                $skills[$row['skill_id']] = sum_ranges($skills[$row['skill_id']], array('min' => $row['min_rank'], 'max' => $row['max_rank'], 'name' => $row['name'], 'player_id' => $row['player_id'], 'sector' => $row['sector_name'])); 
             }
             else
             {
-                $skills[$row['skill_id']] = array(array('min' => $row['min_rank'], 'max' => $row['max_rank'], 'npc' => array($row['player_id']), 'status' => ''));
+                $skills[$row['skill_id']] = array(array('min' => $row['min_rank'], 'max' => $row['max_rank'], 'npc' => array($row['name']), 'status' => '', 'name' => $row['name'], 'player_id' => $row['player_id'], 'sector' => $row['sector_name']));
             }
         }
         
@@ -116,17 +118,16 @@ function checktrainers()
             }
             if(!isset($skills[$row['skill_id']]) || count($skills[$row['skill_id']]) == 0)
             {
-                $status .= '<span style="color: yellow;">No Trainer available for this skill!</span>';
+                $status .= '<span style="color: orange;">No Trainer available for this skill!</span>';
             }
             elseif(count($skills[$row['skill_id']]) > 1)
             {
-                $status .= '<span style="color: yellow;">There are gaps between the skill levels!</span>';
+                $status .= '<span style="color: red;">There are gaps between the skill levels!</span>';
             }
             $status = (empty($status) ? 'OK' : $status);
             echo '<tr class="color_'.$color.'"><td><label title="'.$row['skill_id'].'">'.htmlentities($row['name']).'</label></td><td>'.$str.'</td><td>'.$status.'</td></tr>';
         }
         echo '</table>';
-        
     }
     else
     {
