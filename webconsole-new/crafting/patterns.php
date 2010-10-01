8c30465c1c71a255016b258ff11e0c1f65cd2456
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2010-10-01 16:10:48 +0000

    One can now edit the k_factor of patterns (difficulty factor).

--- webconsole-new/crafting/patterns.php
+++ webconsole-new/crafting/patterns.php
@@ -31,71 +31,89 @@ function listpatterns(){
 }
 
 function editpattern(){
-  if (checkaccess('crafting','read')){
-    echo '<p class="header">Pattern Details</p>';
-    if (isset($_GET['id'])){
-      $pattern_id = mysql_real_escape_string($_GET['id']);
-    }else{
-      echo '<p class="error">No Pattern ID specified</p>';
-      listpatterns();
-      return;
+    if (!checkaccess('crafting','read'))
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+        return;
     }
-    if ((checkaccess('crafting', 'edit')) && isset($_POST['commit']) && ($_POST['commit']=='Update Pattern')){
-      $pattern_name = mysql_real_escape_string($_POST['pattern_name']);
-      $description = mysql_real_escape_string($_POST['description']);
-      $id = mysql_real_escape_string($_GET['id']);
-      $query = "UPDATE trade_patterns SET pattern_name='$pattern_name', description='$description' WHERE id='$id'";
-      $result = mysql_query2($query);
-      echo '<p class="error">Update Successful</p>';
-      unset($_POST);
-      editpattern();
-      return;
-    }else{
-      $query = "SELECT id, name FROM item_stats WHERE stat_type='B'";
-      $Temp = mysql_query2($query);
-      while ($row=mysql_fetch_array($Temp, MYSQL_ASSOC)){
+    echo '<p class="header">Pattern Details</p>';
+    if (isset($_GET['id']))
+    {
+        $pattern_id = mysql_real_escape_string($_GET['id']);
+    }
+    else
+    {
+        echo '<p class="error">No Pattern ID specified</p>';
+        listpatterns();
+        return;
+    }
+    if ((checkaccess('crafting', 'edit')) && isset($_POST['commit']) && ($_POST['commit']=='Update Pattern'))
+    {
+        $pattern_name = mysql_real_escape_string($_POST['pattern_name']);
+        $description = mysql_real_escape_string($_POST['description']);
+        $k_factor = mysql_real_escape_string($_POST['k_factor']);
+        $id = mysql_real_escape_string($_GET['id']);
+        $query = "UPDATE trade_patterns SET pattern_name='$pattern_name', description='$description', k_factor='$k_factor' WHERE id='$id'";
+        $result = mysql_query2($query);
+        echo '<p class="error">Update Successful</p>';
+        unset($_POST);
+        editpattern();
+        return;
+    }
+    $query = "SELECT id, name FROM item_stats WHERE stat_type='B'";
+    $result = mysql_query2($query);
+    while ($row=mysql_fetch_array($result, MYSQL_ASSOC))
+    {
         $iid=$row['id'];
         $Items["$iid"]=$row['name'];
-        $Items[0]="";
-      }
-      if ($pattern_id != 0) // show pattern details only if pattern_id is not 0. (patternless/general knowledge combines/transforms.
-      {
-          $query = "SELECT pattern_name, description, designitem_id FROM trade_patterns WHERE id='$pattern_id'";
-          $result = mysql_query2($query);
-          $row = mysql_fetch_array($result, MYSQL_ASSOC);
-          if (checkaccess('crafting', 'edit')){
+        $Items[0]='';
+    }
+    if ($pattern_id != 0) // show pattern details only if pattern_id is not 0. (patternless/general knowledge combines/transforms.
+    {
+        $query = "SELECT pattern_name, description, designitem_id, k_factor FROM trade_patterns WHERE id='$pattern_id'";
+        $result = mysql_query2($query);
+        $row = mysql_fetch_array($result, MYSQL_ASSOC);
+        if (checkaccess('crafting', 'edit'))
+        {
             $delete_text = (checkaccess('crafting','delete') ? '<a href="./index.php?do=deletepattern&id='.$pattern_id.'">Delete Pattern</a>' : "");
             echo '<form action="./index.php?do=editpattern&amp;id='.$pattern_id.'" method="post">';
             echo '<table><tr><td>Pattern Name:</td><td><input type="text" name="pattern_name" value="'.$row['pattern_name'].'"/></td></tr>';
             echo '<tr><td>Pattern Description:</td><td><textarea name="description" rows="5" cols="40">'.$row['description'].'</textarea></td></tr>';
             $i = $row['designitem_id'];
             echo '<tr><td>Design Item:</td><td>'.$Items["$i"].'</td></tr>';
+            echo '<tr><td>Difficulty Factor:</td><td><input type="text" name="k_factor" value="'.$row['k_factor'].'"/></td></tr>';
             echo '<tr><td><input type="submit" name="commit" value="Update Pattern"/></td><td>'.$delete_text.'</td></tr>';
             echo '</table>';
             echo '</form>';
-          }else{
+        }
+        else
+        {
             echo '<table>';
             echo '<tr><td>Pattern Name:</td><td>'.$row['pattern_name'].'</td></tr>';
             echo '<tr><td>Pattern Description:</td><td>'.$row['description'].'</td></tr>';
             $i = $row['designitem_id'];
             echo '<tr><td>Design Item:</td><td>'.$Items["$i"].'</td></tr>';
-          }
-      }
-      else
-      {
+        }
+    }
+    else
+    {
         echo '<p>Please take note that this is not technically a pattern, it is the lack of it. This collection of combines and transforms is considered "general knowledge" and because of that is not associated with any pattern. You are free to add/edit any of these as long as you keep this in mind.</p>';
-      }
-      echo '<p class="bold">Available Transforms</p>';
-      $query = "SELECT t.id, t.process_id, p.name, t.result_id, i.name AS result_name, c.name AS result_cat, c.category_id AS result_cat_id, t.result_qty, t.item_id, ii.name AS item_name, cc.name AS item_cat, cc.category_id AS item_cat_id, t.item_qty, t.trans_points, t.penalty_pct, t.description FROM trade_transformations AS t LEFT JOIN item_stats AS i ON i.id=t.result_id LEFT JOIN item_stats AS ii ON ii.id=t.item_id LEFT JOIN trade_processes AS p ON t.process_id=p.process_id LEFT JOIN item_categories AS c ON i.category_id=c.category_id LEFT JOIN item_categories AS cc ON ii.category_id=cc.category_id WHERE pattern_id='$pattern_id' GROUP BY id ORDER BY p.name, ii.name, i.name";
-      $result = mysql_query2($query);
-      echo '<table><tr><th colspan="2">Source Item</th><th>Category</th><th>Process</th><th colspan="2">Result Item</th><th>Category</th><th>Time</th><th>Result Q</th><th>Actions</th></tr>';
-      $Alt = FALSE;
-      while ($row=mysql_fetch_array($result, MYSQL_ASSOC)){
-        $Alt = !$Alt;
-        if ($Alt){
-          echo '<tr class="color_a">';
-        }else{
-          echo '<tr class="color_b">';
+    }
+    echo '<p class="bold">Available Transforms</p>';
+    $query = "SELECT t.id, t.process_id, p.name, t.result_id, i.name AS result_name, c.name AS result_cat, c.category_id AS result_cat_id, t.result_qty, t.item_id, ii.name AS item_name, cc.name AS item_cat, cc.category_id AS item_cat_id, t.item_qty, t.trans_points, t.penalty_pct, t.description FROM trade_transformations AS t LEFT JOIN item_stats AS i ON i.id=t.result_id LEFT JOIN item_stats AS ii ON ii.id=t.item_id LEFT JOIN trade_processes AS p ON t.process_id=p.process_id LEFT JOIN item_categories AS c ON i.category_id=c.category_id LEFT JOIN item_categories AS cc ON ii.category_id=cc.category_id WHERE pattern_id='$pattern_id' GROUP BY id ORDER BY p.name, ii.name, i.name";
+    $result = mysql_query2($query);
+    echo '<table><tr><th colspan="2">Source Item</th><th>Category</th><th>Process</th><th colspan="2">Result Item</th><th>Category</th><th>Time</th><th>Result Q</th><th>Actions</th></tr>';
+    $alt = false;
+    while ($row=mysql_fetch_array($result, MYSQL_ASSOC))
+    {
+        $alt = !$alt;
+        if ($alt)
+        {
+            echo '<tr class="color_a">';
+        }
+        else
+        {
+            echo '<tr class="color_b">';
         }
         $item_name = ($row['item_name'] == "NULL" ? ($row['item_id'] != 0 ? "BROKEN" : "") :$row['item_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
         if (checkaccess('items','edit'))
@@ -122,71 +140,78 @@ function editpattern(){
         echo '<td>'.$row['penalty_pct'].'</td>';
         echo '<td><a href="./index.php?do=transform&amp;id='.$row['id'].'">Edit</a></td>';
         echo '</tr>';
-      }
-      echo '</table>';
-      echo '<a href="./index.php?do=createtransform&amp;id='.$pattern_id.'">Create new transform for this pattern </a><br />';
-      echo '<p class="bold">Available Combinations</p>';
-      $Alt = FALSE;
-      $item = -1;
-      $query = "SELECT t.result_id, c.name AS result_cat, c.category_id AS result_cat_id, i.name AS result_name, t.result_qty, t.item_id, ii.name AS item_name, cc.name AS item_cat, cc.category_id AS item_cat_id, t.min_qty, t.max_qty, t.description FROM trade_combinations AS t LEFT JOIN item_stats AS i ON i.id=t.result_id LEFT JOIN item_stats AS ii ON ii.id=t.item_id LEFT JOIN item_categories AS c ON i.category_id=c.category_id LEFT JOIN item_categories AS cc ON ii.category_id=cc.category_id WHERE pattern_id='$pattern_id' ORDER BY i.name";
-      $result = mysql_query2($query);
-      if (mysql_num_rows($result)!= 0){
+    }
+    echo '</table>';
+    echo '<a href="./index.php?do=createtransform&amp;id='.$pattern_id.'">Create new transform for this pattern </a><br />';
+    echo '<p class="bold">Available Combinations</p>';
+    $alt = false;
+    $item = -1;
+    $query = "SELECT t.result_id, c.name AS result_cat, c.category_id AS result_cat_id, i.name AS result_name, t.result_qty, t.item_id, ii.name AS item_name, cc.name AS item_cat, cc.category_id AS item_cat_id, t.min_qty, t.max_qty, t.description FROM trade_combinations AS t LEFT JOIN item_stats AS i ON i.id=t.result_id LEFT JOIN item_stats AS ii ON ii.id=t.item_id LEFT JOIN item_categories AS c ON i.category_id=c.category_id LEFT JOIN item_categories AS cc ON ii.category_id=cc.category_id WHERE pattern_id='$pattern_id' ORDER BY i.name";
+    $result = mysql_query2($query);
+    if (mysql_num_rows($result) != 0)
+    {
         echo '<table><tr><th colspan="2">Result Item</th><th>Category</th><th>Source Items</th><th>Actions</th></tr>';
-        while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){
-          if ($item != $row['result_id']){
-            if ($item != '-1'){
-              echo '</td><td><a href="./index.php?do=editcombine&amp;id='.$item.'&amp;pattern_id='.$_GET['id'].'">Edit</a></td></tr>'."\n";
-            }
-            $item = $row['result_id'];
-            $Alt = !$Alt;
-            if ($Alt){
-              echo '<tr class="color_a">';
-            }else{
-              echo '<tr class="color_b">';
-            }
-            $result_id = $row['result_id'];
-            $result_name = ($row['result_name'] == "NULL" ? ($row['result_id'] != 0 ? "BROKEN" : "") :$row['result_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
-            if (checkaccess('items','edit'))
+        while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
+        {
+            if ($item != $row['result_id'])
             {
-                echo '<td>'.$row['result_qty'].' </td><td> <a href="./index.php?do=listitems&override1&category='.$row['result_cat_id'].'&item='.$row['result_id'].'">'.$result_name.'</a> </td>';
+                if ($item != '-1')
+                {
+                    echo '</td><td><a href="./index.php?do=editcombine&amp;id='.$item.'&amp;pattern_id='.$_GET['id'].'">Edit</a></td></tr>'."\n";
+                }
+                $item = $row['result_id'];
+                $alt = !$alt;
+                if ($alt)
+                {
+                    echo '<tr class="color_a">';
+                }
+                else
+                {
+                    echo '<tr class="color_b">';
+                }
+                $result_id = $row['result_id'];
+                $result_name = ($row['result_name'] == "NULL" ? ($row['result_id'] != 0 ? "BROKEN" : "") :$row['result_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
+                if (checkaccess('items','edit'))
+                {
+                    echo '<td>'.$row['result_qty'].' </td><td> <a href="./index.php?do=listitems&override1&category='.$row['result_cat_id'].'&item='.$row['result_id'].'">'.$result_name.'</a> </td>';
+                }
+                else
+                {
+                    echo '<td>'.$row['result_qty'].' </td><td> '.$result_name.'</td>';
+                }
+                echo '<td>'.$row['result_cat'].'</td>';
+                $item_name = ($row['item_name'] == "NULL" ? ($row['item_id'] != 0 ? "BROKEN" : "") :$row['item_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
+                if (checkaccess('items','edit'))
+                {
+                    echo '<td>'.$row['min_qty'].' to '.$row['max_qty'].' <a href="./index.php?do=listitems&override1&category='.$row['item_cat_id'].'&item='.$row['item_id'].'">'.$item_name.'</a> ('.$row['item_cat'].')';
+                }
+                else
+                {
+                    echo '<td>'.$row['min_qty'].' to '.$row['max_qty'].' '.$item_name.' ('.$row['item_cat'].')';
+                }
             }
             else
             {
-                echo '<td>'.$row['result_qty'].' </td><td> '.$result_name.'</td>';
+                echo '<br/>';
+                $item_name = ($row['item_name'] == "NULL" ? ($row['item_id'] != 0 ? "BROKEN" : "") :$row['item_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
+                if (checkaccess('items','edit'))
+                {
+                    echo $row['min_qty'].' to '.$row['max_qty'].' <a href="./index.php?do=listitems&override1&category='.$row['item_cat_id'].'&item='.$row['item_id'].'">'.$item_name.'</a> ('.$row['item_cat'].')';
+                }
+                else
+                {
+                    echo $row['min_qty'].' to '.$row['max_qty'].' '.$item_name.' ('.$row['item_cat'].')';
+                }
             }
-            echo '<td>'.$row['result_cat'].'</td>';
-            $item_name = ($row['item_name'] == "NULL" ? ($row['item_id'] != 0 ? "BROKEN" : "") :$row['item_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
-            if (checkaccess('items','edit'))
-            {
-                echo '<td>'.$row['min_qty'].' to '.$row['max_qty'].' <a href="./index.php?do=listitems&override1&category='.$row['item_cat_id'].'&item='.$row['item_id'].'">'.$item_name.'</a> ('.$row['item_cat'].')';
-            }
-            else
-            {
-                echo '<td>'.$row['min_qty'].' to '.$row['max_qty'].' '.$item_name.' ('.$row['item_cat'].')';
-            }
-          }else{
-            echo '<br/>';
-            $item_name = ($row['item_name'] == "NULL" ? ($row['item_id'] != 0 ? "BROKEN" : "") :$row['item_name']); // Item name is broken if NULL was returned and ID is not 0, if ID was 0, name is "", else name the name found in the database.
-            if (checkaccess('items','edit'))
-            {
-                echo $row['min_qty'].' to '.$row['max_qty'].' <a href="./index.php?do=listitems&override1&category='.$row['item_cat_id'].'&item='.$row['item_id'].'">'.$item_name.'</a> ('.$row['item_cat'].')';
-            }
-            else
-            {
-                echo $row['min_qty'].' to '.$row['max_qty'].' '.$item_name.' ('.$row['item_cat'].')';
-            }
-          }
         }
         echo '<td><a href="./index.php?do=editcombine&amp;id='.$item.'&amp;pattern_id='.$_GET['id'].'">Edit</a></td></tr></table>';
         echo '<a href="./index.php?do=createcombine&amp;pattern_id='.$pattern_id.'">Create new combine for this pattern </a><br />';
-      }else{
+    }
+    else
+    {
         echo '<p class="error">No available Combines</p>';
         echo '<a href="./index.php?do=createcombine&amp;pattern_id='.$pattern_id.'">Create new combine for this pattern </a><br />';
-      }
     }
-  }else{
-    echo '<p class="error">You are not authorized to use these functions</p>';
-  }
 }
 
 function createpattern() 
