b0fb2bfb24b7ec83063666032b35cac84d9f7d56
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-03-13 19:01:09 +0000

    fixed & => &amp;

--- www/webconsole-new/assets/assetsmain.php
+++ www/webconsole-new/assets/assetsmain.php
@@ -4,8 +4,8 @@ function assetsmain()
     if (checkaccess('assets', 'read'))
     {
         echo '<div class="menu_left">';
-        echo '<a href="./index.php?do=assetsnpc&op=npc">NPCs Used</a> <br/>';
-		echo '<a href="./index.php?do=assetsnpc&op=trait">NPC Traits Used</a> <br/>';
+        echo '<a href="./index.php?do=assetsnpc&amp;op=npc">NPCs Used</a> <br/>';
+		echo '<a href="./index.php?do=assetsnpc&amp;op=trait">NPC Traits Used</a> <br/>';
         echo '<a href="./index.php?do=assetsitem">Items</a> <br/>';
 
         echo '<hr />';
