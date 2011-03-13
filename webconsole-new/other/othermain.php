531980ca03a83f346451cbe10de6a4e75c5e64c7
weltall2 <weltall2@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-03-13 19:01:09 +0000

    fixed & => &amp;

--- webconsole-new/other/othermain.php
+++ webconsole-new/other/othermain.php
@@ -8,7 +8,7 @@ function othermain()
         echo '<a href="./index.php?do=listpetitions">List petitions</a> <br/>';
         echo '<a href="./index.php?do=listaccounts">List accounts</a> <br/>';
         echo '<a href="./index.php?do=listcharacters">List characters</a> <br/>';
-        echo '<a href="./index.php?do=liststats&groupid=1">List Stats</a> <br/>';
+        echo '<a href="./index.php?do=liststats&amp;groupid=1">List Stats</a> <br/>';
         echo '<a href="./index.php?do=listtraits">List traits</a> <br/>'; 
         echo '<a href="./index.php?do=showraces">Traits per race</a> <br/>'; 
         echo '<a href="./index.php?do=events">List GM Events</a><br/>';
