3b94bf358d457d8ff6dd324c600b93b129c2f548
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-03-18 15:06:31 +0000

    Removing the old WC from the SVN tree. (This version of the WC is 2+ years out of service already, if you are still using it, switch to webconsole-new.)

--- www/webconsole/effects.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Snatched from LWW
-$mouse_over = "onmouseover=\"this.style.background='#012A2A'\"".
-				"onmouseout=\"this.style.background=''\"".
-				"onclick=\"this.style.background=''\"";
-
-?>
