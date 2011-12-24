81f0057f7e4e2eb13cce3d8f13cddfbd3fba30c9
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2011-12-24 02:26:51 +0000

    Added editing of tribe recipes.

--- webconsole-new/tribes/listrecipes.php
+++ webconsole-new/tribes/listrecipes.php
@@ -49,12 +49,21 @@ function listrecipes()
             echo '<td>'.str_replace(';', '; ', $row['algorithm']).'</td>';
             echo '<td>'.$row['persistent'].'</td>';
             echo '<td>'.$row['uniqueness'].'</td>';
-            if (checkaccess('npcs', 'delete'))
+            if (checkaccess('npcs', 'edit'))
             {
                 echo '<td><form action="./index.php?do=editrecipes" method="post">';
                 echo '<input type="hidden" name="id" value="'.$row['id'].'" />';
-                echo '<input type="submit" name="commit" value="Delete" />';
-                echo '</form></td>';
+                echo '<input type="submit" name="commit" value="Edit" />';
+                echo '</form>';
+                
+                if (checkaccess('npcs', 'delete'))
+                {
+                    echo '<form action="./index.php?do=editrecipes" method="post">';
+                    echo '<input type="hidden" name="id" value="'.$row['id'].'" />';
+                    echo '<input type="submit" name="commit" value="Delete" />';
+                    echo '</form>';
+                }
+                echo '</td>';
             }
             echo '</tr>';
         }
@@ -62,16 +71,16 @@ function listrecipes()
         if (checkaccess('npcs', 'create')) 
         {
             echo '<hr />';
-            echo '<p>Create new Tribe Recipe: </p>';
+            echo '<p>Create New Tribe Recipe: </p>';
             echo '<form action="./index.php?do=editrecipes" method="post">';
             echo '<table border="1">';
             echo '<tr><th>Field</th><th>Value</th></tr>';
-            echo '<tr><td>Name</td><td><input type="text" name="name"></td></tr>';
-            echo '<tr><td>Requirements</td><td><input type="text" name="requirements"></td></tr>';
-            echo '<tr><td>Algorithm</td><td><input type="text" name="algorithm"></td></tr>';
-            echo '<tr><td>Persistent</td><td><input type="checkbox" name="persistent"></td></tr>';
-            echo '<tr><td>Uniqueness</td><td><input type="checkbox" name="uniqueness"></td></tr>';
-            echo '<tr><td colspan="2"><input type="submit" name="commit" value="Create Tribe Recipe"></td></tr>';
+            echo '<tr><td>Name</td><td><input type="text" name="name" /></td></tr>';
+            echo '<tr><td>Requirements</td><td><input type="text" name="requirements" /></td></tr>';
+            echo '<tr><td>Algorithm</td><td><input type="text" name="algorithm" /></td></tr>';
+            echo '<tr><td>Persistent</td><td><input type="checkbox" name="persistent" /></td></tr>';
+            echo '<tr><td>Uniqueness</td><td><input type="checkbox" name="uniqueness" /></td></tr>';
+            echo '<tr><td colspan="2"><input type="submit" name="commit" value="Create Tribe Recipe" /></td></tr>';
             echo '</table>';
             echo '</form>';
         }
@@ -89,9 +98,9 @@ function editrecipes()
         echo '<p class="error">You are not authorized to use these functions</p>';
         return;
     }
-    if (!isset($_POST['commit'])) // Show edit form will come here, placeholder.
+    if (!isset($_POST['commit'])) 
     {
-        echo '<p class="error">Invalid call.</p>';
+        echo '<p class="error">Invalid Instruction</p>';
         return;
     }
     
@@ -130,6 +139,58 @@ function editrecipes()
         $query = "DELETE FROM tribe_recipes WHERE id='$id' LIMIT 1";
         mysql_query2($query);
         echo '<p class="error">Tribe Recipe With ID '.$id.' Successfully Deleted</p>';
+        unset($_POST);
+        listrecipes();
+        return;
+    }
+    else if ($_POST['commit'] == 'Edit')
+    {
+        if (!isset($_POST['id']) || !is_numeric($_POST['id']))
+        {
+            echo '<p class="error">Invalid ID, cannot edit.</p>';
+            return;
+        }
+        $id = mysql_real_escape_string($_POST['id']);
+        $query = "SELECT * FROM tribe_recipes WHERE id='$id'";
+        $result = mysql_query2($query);
+        if (mysql_num_rows($result) == 0)
+        {
+            echo '<p class="error">No database entry with ID '.$id.', cannot edit</p>';
+            return;
+        }
+        $row = mysql_fetch_array($result);
+        echo '<p>Edit Tribe Recipe With ID '.$id.' </p>';
+        echo '<form action="./index.php?do=editrecipes" method="post">';
+        echo '<table border="1">';
+        echo '<tr><th>Field</th><th>Value</th></tr>';
+        echo '<tr><td>Name</td><td><input type="text" name="name" value="'.$row['name'].'" /></td></tr>';
+        echo '<tr><td>Requirements</td><td><input type="text" name="requirements" value="'.$row['requirements'].'" /></td></tr>';
+        echo '<tr><td>Algorithm</td><td><input type="text" name="algorithm" value="'.$row['algorithm'].'" /></td></tr>';
+        $persistent = ($row['persistent'] == 1 ? 'checked="checked"' : '');
+        echo '<tr><td>Persistent</td><td><input type="checkbox" name="persistent" '.$persistent.'" /></td></tr>';
+        $uniqueness = ($row['uniqueness'] == 1 ? 'checked="checked"' : '');
+        echo '<tr><td>Uniqueness</td><td><input type="checkbox" name="uniqueness" '.$uniqueness.'" /></td></tr>';
+        echo '<tr><td colspan="2"><input type="hidden" name="id" value="'.$id.'" /><input type="submit" name="commit" value="Update" /></td></tr>';
+        echo '</table>';
+        echo '</form>';
+        return;
+    }
+    else if ($_POST['commit'] == 'Update')
+    {
+        if (!isset($_POST['id']) || !is_numeric($_POST['id']))
+        {
+            echo '<p class="error">Invalid ID, cannot commit edit.</p>';
+            return;
+        }
+        $id = mysql_real_escape_string($_POST['id']);
+        $name = mysql_real_escape_string($_POST['name']);
+        $requirements = mysql_real_escape_string($_POST['requirements']);
+        $algorithm = mysql_real_escape_string($_POST['algorithm']);
+        $persistent = (isset($_POST['persistent']) ? 1 : 0);
+        $uniqueness = (isset($_POST['uniqueness']) ? 1 : 0);
+        $query = "UPDATE tribe_recipes SET name='$name', requirements='$requirements', algorithm='$algorithm', persistent='$persistent', uniqueness='$uniqueness' WHERE id='$id'";
+        mysql_query2($query);
+        echo '<p class="error">Tribe Recipe Successfully Updated</p>';
         unset($_POST);
         listrecipes();
         return;
