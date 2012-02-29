cdab6cabf79faecb9bbcfb4e9432be55ccde355a
psshared <psshared@2752fbe2-5038-0410-9d0a-88578062bcef>
2012-02-29 18:26:52 +0000

    update logs listing source

--- gmconsole/logs.php
+++ gmconsole/logs.php
@@ -7,7 +7,7 @@
 	
 	// is the user logged in?
 	if (!isset($_SESSION["__SECURITY_LEVEL"])) {
-		header("Location: index.php?origin=logs");
+		header("Location: index.php?origin=reportlogs");
 		exit;
 	}
 
@@ -15,11 +15,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
-		<title>Private server log (refreshed every few minutes)</title>
+		<title>Server status</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" href="<?=$__CSS_RELURI?>" media="all"/>
         <link rel="stylesheet" type="text/css" href="<?=$__CSS_ADDON_RELURI?>" media="all"/>
-	</head>
+        <script src="sorttable.js"></script>
+</head>
 	<body>
 		<table>
 			<tr>
@@ -29,18 +30,23 @@
 ?>
 				</td>
 				<td style="vertical-align:top;">
-                        <h2 class="yellowtitlebig">Log</h2>
-
+                        <h2 class="yellowtitlebig">Server Status</h2>
+                        Click the columns to sort.
 <?php
-							$file = fopen("/home/planeshift/psserver/planeshift/report.xml", "r");
-							while(!feof($file))
-							{
-								echo htmlentities(fgets($file))."<br>";
-							}
-							fclose($file);
+$report = simplexml_load_file("/home/planeshift/psserver/planeshift/report.xml");
+echo "<table class=\"sortable\">";
+echo "<tr><th>name</th><th>characterID</th><th>guild</th><th>title</th><th>security</th><th>kills</th><th>deaths</th><th>suicides</th><th>X</th><th>Y</th><th>Z</th><th>sector</th></tr>";
+foreach($report->player as $player) {
+    echo "<tr><td>", $player['name'], "</td><td>", $player['characterID'], "</td><td>", $player['guild'], "</td><td>", $player['title'], "</td><td>", $player['security'], "</td><td>", $player['kills'], "</td><td>", $player['deaths'], "</td><td>", $player['suicides'], "</td><td>", $player['pos_x'], "</td><td>", $player['pos_y'], "</td><td>", $player['pos_z'], "</td><td>", $player['sector'], "</td></tr>";
+}
+echo "</table>";
+echo "<table class=\"sortable\">";
+echo "<tr><th>name</th><th>characterID</th><th>kills</th><th>deaths</th><th>suicides</th><th>X</th><th>Y</th><th>Z</th><th>sector</th></tr>";
+foreach($report->npc as $player) {
+    echo "<tr><td>", $player['name'], "</td><td>", $player['characterID'], "</td><td>", $player['kills'], "</td><td>", $player['deaths'], "</td><td>", $player['suicides'], "</td><td>", $player['pos_x'], "</td><td>", $player['pos_y'], "</td><td>", $player['pos_z'], "</td><td>", $player['sector'], "</td></tr>";
+}
+echo "</table>"
+
 ?>
-				</td>
-			</tr>
-		<table>
 	</body>
 </html>
