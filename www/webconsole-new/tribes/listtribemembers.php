fda85a7476543e08c2bec8cb6789c227e201ef25
magodra <magodra@2752fbe2-5038-0410-9d0a-88578062bcef>
2013-02-10 17:40:10 +0000

    - Added display of tribe_members flags.

--- www/webconsole-new/tribes/listtribemembers.php
+++ www/webconsole-new/tribes/listtribemembers.php
@@ -8,7 +8,7 @@ function listtribemembers()
         return;
     }
     
-    $query = 'SELECT tm.tribe_id, t.name AS tribe_name, tm.member_id, tm.member_type, c.name FROM tribe_members AS tm LEFT JOIN characters AS c ON c.id=tm.member_id LEFT JOIN tribes AS t ON t.id=tm.tribe_id';
+    $query = 'SELECT tm.tribe_id, t.name AS tribe_name, tm.member_id, tm.member_type, c.name, tm.flags FROM tribe_members AS tm LEFT JOIN characters AS c ON c.id=tm.member_id LEFT JOIN tribes AS t ON t.id=tm.tribe_id';
 
     if (isset($_GET['tribe_id']) && is_numeric($_GET['tribe_id'])) 
     {
@@ -22,7 +22,7 @@ function listtribemembers()
     if (mysql_num_rows($result) > 0)
     {
         echo '<table border="1">';
-        echo '<tr><th>Tribe</th><th>Member Name</th><th>Member Type</th></tr>';
+        echo '<tr><th>Tribe</th><th>Member Name</th><th>Member Type</th><th>Flags</th></tr>';
         
         while ($row = mysql_fetch_array($result))
         {
@@ -30,6 +30,7 @@ function listtribemembers()
             echo '<td>'.$row['tribe_name'].'</td>';
             echo '<td><a href="./index.php?do=npc_details&amp;sub=main&amp;npc_id='.$row['member_id'].'">'.$row['name'].'</a></td>';
             echo '<td>'.$row['member_type'].'</td>';
+            echo '<td>'.$row['flags'].'</td>';
             echo '</tr>';
         }
         echo '</table>';
