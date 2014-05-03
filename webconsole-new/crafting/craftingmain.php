7f2a3ed5c2e5506d57d8d18a7510cef8e7893c26
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-03 19:29:49 +0000

    Fixed a minor php bug. Added more readability to the source code. Improved bad data handling.

--- webconsole-new/crafting/craftingmain.php
+++ webconsole-new/crafting/craftingmain.php
@@ -5,25 +5,25 @@ function craftingmain()
 {
     if (checkaccess('crafting', 'read'))
     {
-        echo '<div class="menu_left">';
-        echo '<a href="./index.php?do=listcraftitems">List Craftable Items</a> <br/>';
-		echo '<a href="./index.php?do=checkminditemusage">Check Mind Item Usage</a> <br/>';
-        echo '<a href="./index.php?do=listpatterns">List Patterns</a> <br/>';
-        echo '<a href="./index.php?do=listprocess">List Process</a> <br/>';
+        echo '<div class="menu_left">'."\n";
+        echo '<a href="./index.php?do=listcraftitems">List Craftable Items</a> <br/>'."\n";
+		echo '<a href="./index.php?do=checkminditemusage">Check Mind Item Usage</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listpatterns">List Patterns</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listprocess">List Process</a> <br/>'."\n";
         if (checkaccess('crafting', 'create'))
         {
-            echo '<a href="./index.php?do=createtransform">Create Transform</a> <br/>';
-            echo '<a href="./index.php?do=createpattern">Create Pattern</a> <br/>';
+            echo '<a href="./index.php?do=createtransform">Create Transform</a> <br/>'."\n";
+            echo '<a href="./index.php?do=createpattern">Create Pattern</a> <br/>'."\n";
         }
-        echo '<hr/><a href="./index.php?do=resource">List Natural Resources</a><br/>';
-        echo '<a href="./index.php?do=resourcemap">List Resource Map</a><br/>';
-        echo '<a href="./index.php?do=huntlocations">List Hunt Locations</a><br/>';
-        echo '<hr/><a href="./index.php">Return to main page.</a>';
-        echo '</div><div class="main">';
+        echo '<hr/><a href="./index.php?do=resource">List Natural Resources</a><br/>'."\n";
+        echo '<a href="./index.php?do=resourcemap">List Resource Map</a><br/>'."\n";
+        echo '<a href="./index.php?do=huntlocations">List Hunt Locations</a><br/>'."\n";
+        echo '<hr/><a href="./index.php">Return to main page.</a>'."\n";
+        echo '</div><div class="main">'."\n";
     }
     else
     {
-        echo 'You are not authorized to use these functions!';
+        echo 'You are not authorized to use these functions!'."\n";
     }
 }
 ?>
