0c96018bbbdf75ae98b2cfd8d3d83a46530f49db
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-03 00:27:49 +0000

    Fixed various html bugs in "find word in KA". Added a "find response in trigger" to the "find word in KA".

--- www/webconsole-new/npcs/findtrigger.php
+++ www/webconsole-new/npcs/findtrigger.php
@@ -3,9 +3,25 @@ function findtrigger(){
 
     if (checkaccess('npcs', 'read') && isset($_POST['commit']))
     {
-        $word = $_POST['word'];
+        $word = mysql_real_escape_string($_POST['word']);
 
-        $query = "SELECT id,trigger_text,area FROM npc_triggers WHERE trigger_text='$word' OR trigger_text LIKE '% $word %' OR trigger_text LIKE '$word %' OR trigger_text LIKE '% $word'";
+        if ($_POST['commit'] == 'Search Trigger')
+        {
+            $query = "SELECT id, trigger_text, area FROM npc_triggers WHERE trigger_text='$word' OR trigger_text LIKE '% $word %' OR trigger_text LIKE '$word %' OR trigger_text LIKE '% $word'";
+        }
+        else if ($_POST['commit'] == 'Search Response')
+        {
+            $query = "SELECT nt.id, trigger_text, area FROM npc_triggers AS nt LEFT JOIN npc_responses AS nr ON nt.id=trigger_id WHERE response1='$word' OR response1 LIKE '% $word %' OR response1 LIKE '$word %' OR response1 LIKE '% $word'";
+            $query .= " OR response2='$word' OR response2 LIKE '% $word %' OR response2 LIKE '$word %' OR response2 LIKE '% $word'";
+            $query .= " OR response3='$word' OR response3 LIKE '% $word %' OR response3 LIKE '$word %' OR response3 LIKE '% $word'";
+            $query .= " OR response4='$word' OR response4 LIKE '% $word %' OR response4 LIKE '$word %' OR response4 LIKE '% $word'";
+            $query .= " OR response5='$word' OR response5 LIKE '% $word %' OR response5 LIKE '$word %' OR response5 LIKE '% $word'";
+        }
+        else
+        {
+            echo '<p class="error">Should not reach this.</p>';
+            return;
+        }
         $result = mysql_query2($query);
 
         if (mysql_num_rows($result) == 0)
@@ -13,22 +29,28 @@ function findtrigger(){
             echo '<p class="error">No results were found.</p>';
             return;
         }
-        echo '<b>Triggers found</b><br><br>';
-        echo '<table border=1><th>ID</th><th>Trigger</th><th>Area</th>';
+        echo '<b>Triggers found</b><br /><br />';
+        echo '<table border="1"><tr><th>ID</th><th>Trigger</th><th>Area</th></tr>';
         while ($line = mysql_fetch_array($result, MYSQL_NUM)){
-            echo '<TR><TD><b>'.$line[0].'</b></TD><TD>'.$line[1].'</TD><TD><a href="./index.php?do=ka_detail&area='.$line[2].'">'.$line[2].'</a></TD></TR>';
+            echo '<tr><td><b>'.htmlentities($line[0]).'</b></td><td>'.htmlentities($line[1]).'</td><td><a href="./index.php?do=ka_detail&amp;area='.htmlentities($line[2]).'">'.htmlentities($line[2]).'</a></td></tr>';
         }
-        echo '</TABLE><br>';
+        echo '</table>';
     }
     else if (checkaccess('npcs', 'read'))
     {
-        echo '<FORM ACTION="./index.php?do=findtrigger" METHOD="POST" >';
-        echo '<br><TABLE BORDER=0>';
-        echo '<tr><td>Enter the word to search for: </td><td><INPUT TYPE=text NAME=word>';
-        echo '<INPUT TYPE="SUBMIT" NAME="commit" VALUE="Search"></td></tr>';
-        echo '</TABLE></FORM>';
-
-    echo '<br><br>';
+        echo '<p>Search Trigger</p>';
+        echo '<form action="./index.php?do=findtrigger" method="post" >';
+        echo '<table border="0">';
+        echo '<tr><td>Enter the word to search for: </td><td><input type="text" name="word" />';
+        echo '<input type="submit" name="commit" value="Search Trigger" /></td></tr>';
+        echo '</table></form>';
+        
+        echo '<p>Search response</p>';
+        echo '<form action="./index.php?do=findtrigger" method="post" >';
+        echo '<table border="0">';
+        echo '<tr><td>Enter the word to search for: </td><td><input type="text" name="word" />';
+        echo '<input type="submit" name="commit" value="Search Response" /></td></tr>';
+        echo '</table></form>';
     }
     else
     {
