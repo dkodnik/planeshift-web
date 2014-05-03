4a268b155c17057d2ac338412cdb6d786ecf5e00
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-03 20:34:27 +0000

    Changed the list_npc_quests to only check for racegenderID in the main listing. This allows NPCs like Zak to still be displayed when their quests are accessed through the NPC -> Quests pages. Added some readability to html in questmain.

--- webconsole-new/quests/questmain.php
+++ webconsole-new/quests/questmain.php
@@ -3,17 +3,17 @@
 
 function questmain(){
   if (checkaccess('quests', 'read')){
-    echo '<div class="menu_left">';
-    echo '<a href="./index.php?do=listquests">List Quests</a> <br/>';
+    echo '<div class="menu_left">'."\n";
+    echo '<a href="./index.php?do=listquests">List Quests</a> <br/>'."\n";
     if (checkaccess('quests', 'create')){
-      echo '<a href="./index.php?do=createquest">Create Quest</a> <br/>';
+      echo '<a href="./index.php?do=createquest">Create Quest</a> <br/>'."\n";
     }
-    echo '<a href="./index.php?do=npcquests">List By NPC</a> <br/>';
-    echo '<a href="./index.php">Return to main page.</a>';
-    echo '</div><div class="main">';
+    echo '<a href="./index.php?do=npcquests">List By NPC</a> <br/>'."\n";
+    echo '<a href="./index.php">Return to main page.</a>'."\n";
+    echo '</div><div class="main">'."\n";
   }
   else{
-    echo 'You are not authorized to use these functions!';
+    echo 'You are not authorized to use these functions!'."\n";
   }
 }
 ?>
