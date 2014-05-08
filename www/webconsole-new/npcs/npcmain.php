b90055f13fd73c34b457b58eb33ecef27c18c2d4
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2014-05-08 22:17:10 +0000

    Prevented duplicate IDs from showing in results for find triggers. Made a direct link to edit trigger in the search results. Added the option to show all responses in the results. Added some more readability to the html source.

--- www/webconsole-new/npcs/npcmain.php
+++ www/webconsole-new/npcs/npcmain.php
@@ -4,41 +4,41 @@
 function npcmain(){
     if (checkaccess('npcs', 'read'))
     {
-        echo '<div class="menu_left">';
-        echo '<a href="./index.php?do=searchnpc&amp;char_type=1">Search for NPCs</a> <br/>';
-        echo '<a href="./index.php?do=searchnpc&amp;char_type=0">Search for Players</a> <br/>';
-        echo '<a href="./index.php?do=searchnpc&amp;char_type=2">Search for Pets</a> <br/>';
-        echo '<a href="./index.php?do=searchnpc&amp;char_type=3">Search for Mounts</a> <br/>';
+        echo '<div class="menu_left">'."\n";
+        echo '<a href="./index.php?do=searchnpc&amp;char_type=1">Search for NPCs</a> <br/>'."\n";
+        echo '<a href="./index.php?do=searchnpc&amp;char_type=0">Search for Players</a> <br/>'."\n";
+        echo '<a href="./index.php?do=searchnpc&amp;char_type=2">Search for Pets</a> <br/>'."\n";
+        echo '<a href="./index.php?do=searchnpc&amp;char_type=3">Search for Mounts</a> <br/>'."\n";
+        echo '<hr/>'."\n";
+        echo '<a href="./index.php?do=createnpc">Create NPC</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listnpcs">List NPCs (invuln)</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listvuln">List NPCs (vuln)</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listnpcscombat">List NPCs (combat view)</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listnpcsector">List Invul. NPCs by Sector</a> <br/>'."\n";
+        echo '<a href="./index.php?do=viewnpcmap">View NPC Map</a><br/>'."\n";
+        echo '<a href="./index.php?do=listnpctypes&amp;template=1">List NPC Type Templates</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listnpctypes">List NPC Types</a> <br/>'."\n";
+        echo '<hr/>'."\n";
+        echo '<a href="./index.php?do=listtrainer">List Trainers</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listmerchant">List Merchants</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listspawn">List Spawn Rules</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listloot">List Loot Rules</a> <br/>'."\n";
         echo '<hr/>';
-        echo '<a href="./index.php?do=createnpc">Create NPC</a> <br/>';
-        echo '<a href="./index.php?do=listnpcs">List NPCs (invuln)</a> <br/>';
-        echo '<a href="./index.php?do=listvuln">List NPCs (vuln)</a> <br/>';
-        echo '<a href="./index.php?do=listnpcscombat">List NPCs (combat view)</a> <br/>';
-        echo '<a href="./index.php?do=listnpcsector">List Invul. NPCs by Sector</a> <br/>';
-        echo '<a href="./index.php?do=viewnpcmap">View NPC Map</a><br/>';
-        echo '<a href="./index.php?do=listnpctypes&amp;template=1">List NPC Type Templates</a> <br/>';
-        echo '<a href="./index.php?do=listnpctypes">List NPC Types</a> <br/>';
-        echo '<hr/>';
-        echo '<a href="./index.php?do=listtrainer">List Trainers</a> <br/>';
-        echo '<a href="./index.php?do=listmerchant">List Merchants</a> <br/>';
-        echo '<a href="./index.php?do=listspawn">List Spawn Rules</a> <br/>';
-        echo '<a href="./index.php?do=listloot">List Loot Rules</a> <br/>';
-        echo '<hr/>';
-        echo '<a href="./index.php?do=synonyms">List Synonyms</a> <br/>';
-        echo '<a href="./index.php?do=ka_trigg">List Trigger KAs</a> <br/>';
-        echo '<a href="./index.php?do=ka_scripts">List KA Scripts</a> <br/>';
-        echo '<a href="./index.php?do=findtrigger">Find word in KA</a> <br/>';
-        echo '<hr/>';
-        echo '<a href="./index.php?do=checknpctriggers">Check NPC Triggers</a> <br/>';
-        echo '<a href="./index.php?do=checknpcchar">List NPCs and Base Dialog</a> <br/>';
-        echo '<a href="./index.php?do=checknpcloaded">Check NPC Loaded</a> <br/>';
-        echo '<a href="./index.php?do=checktrainers">Check NPC Trainers</a> <br/>';
-        echo '<hr/><a href="./index.php">Return to main page.</a>';
+        echo '<a href="./index.php?do=synonyms">List Synonyms</a> <br/>'."\n";
+        echo '<a href="./index.php?do=ka_trigg">List Trigger KAs</a> <br/>'."\n";
+        echo '<a href="./index.php?do=ka_scripts">List KA Scripts</a> <br/>'."\n";
+        echo '<a href="./index.php?do=findtrigger">Find word in KA</a> <br/>'."\n";
+        echo '<hr/>'."\n";
+        echo '<a href="./index.php?do=checknpctriggers">Check NPC Triggers</a> <br/>'."\n";
+        echo '<a href="./index.php?do=checknpcchar">List NPCs and Base Dialog</a> <br/>'."\n";
+        echo '<a href="./index.php?do=checknpcloaded">Check NPC Loaded</a> <br/>'."\n";
+        echo '<a href="./index.php?do=checktrainers">Check NPC Trainers</a> <br/>'."\n";
+        echo '<hr/><a href="./index.php">Return to main page.</a>'."\n";
         echo '</div><div class="main">'."\n";
     }
     else
     {
-        echo 'You are not authorized to use these functions!';
+        echo 'You are not authorized to use these functions!'."\n";
     }
 }
 ?>
