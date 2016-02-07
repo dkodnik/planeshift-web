aa9487abf65c7918350a7241f8f21e64f2bbc318
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-02-07 13:05:08 +0000

    changed compareitems to a table with floating table headers and "frozen" first and second columns. added a few linebreaks to itemmain for html source readability. changed all "em" into px for table padding in the style sheet, due to that being more defined. set padding to the same values for TH and TD tags in the style sheet.

--- webconsole-new/items/compareitems.php
+++ webconsole-new/items/compareitems.php
@@ -1,128 +1,187 @@
-<script language="javascript" type="text/javascript">
-function comparesortby(val)
-{
-	if(document.getElementById('comparesort').value == val)
-	{
-		document.getElementById('comparesort').value=val + ' desc';
-		document.getElementById('compareform').submit();
-	}
-	else
-	{
-		document.getElementById('comparesort').value=val;
-		document.getElementById('compareform').submit();
-	}
-}
-</script>
 <?php
 function compareitems(){
-  if(checkaccess('items', 'read')){
-	$cat = '';
-	if (isset($_POST['category_id'])){
-      $cat = $_POST['category_id'];
+    if(!checkaccess('items', 'read'))
+    {
+        echo '<p class="error">You are not authorized to use these functions!</p>';
+        return;
     }
-	$comparetype = '';
-	$comtype = '';
-	if (isset($_POST['compare_type']))
-	{
-	  $comtype = $_POST['compare_type'];
-	}
-	else
-	{
-		$comtype = 'gen';
-	}
-	$comsort = '';
-	if (isset($_POST['comparesort']))
-	{
-	  $comsort = $_POST['comparesort'];
-	}
-	else
-	{
-		$comsort = 'name';
-	}
-	$tableheads = '<th><a href="#" onclick="comparesortby(\'id\');">ID</a></th>
-	<th><a href="#" onclick="comparesortby(\'name\');">Name</a></th>';
-	$selvals = 'i.id, i.name';
-	
-	  switch($comtype)
-	 {
-		 
-		case 'att':
-			$comparetype = '<option value="att">Attack</option><option value="gen">General</option><option value="def">Defense</option><option value="gfx">Graphics</option><option value="req">Requirements</option><option value="bon">Bonuses</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'dmg_blunt\');">Blunt</a></th><th><a href="#" onclick="comparesortby(\'dmg_pierce\');">Pierce</a></th><th><a href="#" onclick="comparesortby(\'dmg_slash\');">Slash</a></th><th><a href="#" onclick="comparesortby(\'ammo\');">Ammo</a></th><th><a href="#" onclick="comparesortby(\'weapon_penetration\');">Penetration</a></th><th><a href="#" onclick="comparesortby(\'weapon_range\');">Range</a></th><th><a href="#" onclick="comparesortby(\'weapon_speed\');">Speed</a></th>';
-			$selvals .= ', i.dmg_blunt, i.dmg_pierce, i.dmg_slash, i2.name as ammo, i.weapon_penetration, i.weapon_range, i.weapon_speed';
-			break;
-		case 'def':
-			$comparetype = '<option value="def">Defense</option><option value="gen">General</option><option value="att">Attack</option><option value="gfx">Graphics</option><option value="req">Requirements</option><option value="bon">Bonuses</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'armor_hardness\');">Armor Hardness</a></th><th><a href="#" onclick="comparesortby(\'armorvsweapon_type\');">Armor Vs Weapon</a></th><th><a href="#" onclick="comparesortby(\'weapon_block_targeted\');">Block Targeted</a></th><th><a href="#" onclick="comparesortby(\'weapon_block_untargeted\');">Block Untargeted</a></th><th><a href="#" onclick="comparesortby(\'weapon_counterblock\');">Counter Block</a></th>';
-			$selvals .= ', i.armor_hardness, i.armorvsweapon_type, i.weapon_block_targeted, i.weapon_block_untargeted, i.weapon_counterblock';
-			break;
-		case 'gfx':
-			$comparetype = '<option value="gfx">Graphics</option><option value="gen">General</option><option value="att">Attack</option><option value="def">Defense</option><option value="req">Requirements</option><option value="bon">Bonuses</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'cstr_gfx_icon\');">Icon</a></th><th><a href="#" onclick="comparesortby(\'cstr_gfx_mesh\');">Mesh</a></th><th><a href="#" onclick="comparesortby(\'cstr_gfx_texture\');">Texture</a></th><th><a href="#" onclick="comparesortby(\'cstr_part\');">CSTR Part</a></th><th><a href="#" onclick="comparesortby(\'cstr_part_mesh\');">CSTR Mesh</a></th><th><a href="#" onclick="comparesortby(\'removed_mesh\');">Removed Mesh</a></th>';
-			$selvals .= ', i.cstr_gfx_icon, i.cstr_gfx_mesh, i.cstr_gfx_texture, i.cstr_part, i.cstr_part_mesh, i.removed_mesh';
-			break;
-		case 'req':
-			$comparetype = '<option value="req">Requirements</option><option value="gen">General</option><option value="att">Attack</option><option value="def">Defense</option><option value="gfx">Graphics</option><option value="bon">Bonuses</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'requirement1\');">Requirement 1</a></th><th><a href="#" onclick="comparesortby(\'requirement2\');">Requirement 2</a></th><th><a href="#" onclick="comparesortby(\'requirement3\');">Requirement 3</a></th>';
-			$selvals .= ', CONCAT(i.requirement_1_name,"=",i.requirement_1_value) as requirement1, CONCAT(i.requirement_2_name, "=",i.requirement_2_value) as requirement2, CONCAT(i.requirement_3_name,"=",i.requirement_3_value) as requirement3';
-			break;
-		case 'bon':
-			$comparetype = '<option value="bon">Bonuses</option><option value="gen">General</option><option value="att">Attack</option><option value="def">Defense</option><option value="gfx">Graphics</option><option value="req">Requirements</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'item_bonus_1\');">Bonus 1</a></th><th><a href="#" onclick="comparesortby(\'item_bonus_2\');">Bonus 2</a></th><th><a href="#" onclick="comparesortby(\'item_bonus_3\');">Bonus 3</a></th>';
-			$selvals .= ', CONCAT(i.item_bonus_1_attr,"=",i.item_bonus_1_max) as item_bonus_1, CONCAT(i.item_bonus_2_attr,"=", i.item_bonus_2_max) as item_bonus_2, CONCAT(i.item_bonus_3_attr,"=",i.item_bonus_3_max) as item_bonus_3';
-			break;
-		case 'misc':
-			$comparetype = '<option value="misc">Misc</option><option value="gen">General</option><option value="att">Attack</option><option value="def">Defense</option><option value="gfx">Graphics</option><option value="req">Requirements</option><option value="bon">Bonuses</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'description\');">Description</a></th><th><a href="#" onclick="comparesortby(\'spawnable\');">Spawnable</a></th><th><a href="#" onclick="comparesortby(\'item_skills\');">Skills</a></th><th><a href="#" onclick="comparesortby(\'consume_script\');">Consume</a></th><th><a href="#" onclick="comparesortby(\'equip_script\');">Equip</a></th>';
-			$selvals .= ', i.description, i.spawnable, CONCAT_WS(",",s1.name,", ",s2.name,", ",s3.name) as item_skills, i.consume_script, i.equip_script';
-			break;
-		default:
-			$comparetype = '<option value="gen">General</option><option value="att">Attack</option><option value="def">Defense</option><option value="gfx">Graphics</option><option value="req">Requirements</option><option value="bon">Bonuses</option><option value="misc">Misc</option>';
-			$tableheads .= '<th><a href="#" onclick="comparesortby(\'size\');">Size</a></th><th><a href="#" onclick="comparesortby(\'weight\');">Weight</a></th><th><a href="#" onclick="comparesortby(\'base_sale_price\');">Price</a></th><th><a href="#" onclick="comparesortby(\'container\');">Container</a></th><th><a href="#" onclick="comparesortby(\'item_max_quality\');">Quality</a></th><th><a href="#" onclick="comparesortby(\'valid_slots\');">Slot</a></th><th><a href="#" onclick="comparesortby(\'flags\');">Flags</a></th>';
-			$selvals .= ', i.size, i.weight, i.base_sale_price, (i.container_max_size +"-"+i.container_max_slots) as container, i.item_max_quality, i.valid_slots, i.flags';
-   	 }
-	 
-	 echo('<form method="post" action="index.php?do=compareitems" id="compareform">');
-	$categories = PrepSelect('category');
-	echo(DrawSelectBox('category', $categories, 'category_id', $cat));
-	echo('<select id="comparetype" name="compare_type" value="' . $comtype . '" onchange="document.getElementById(\'comparesort\').value=\'name\'">' . $comparetype . '</select>');
-    echo('<input type="hidden" id="comparesort" name="comparesort" value="' . $comsort . '"><input type="submit" value="View"></form>');
-	
-	$tablerows = '';
-	if($cat)
-	{
-		$query = 'SELECT ' . $selvals . ' FROM item_stats i LEFT JOIN skills AS s1 ON i.item_skill_id_1=s1.skill_id LEFT JOIN skills AS s2 ON i.item_skill_id_2=s2.skill_id LEFT JOIN skills AS s3 ON i.item_skill_id_3=s3.skill_id LEFT JOIN item_stats AS i2 ON i.item_type_id_ammo=i2.id WHERE i.category_id="'.$cat.'" ORDER by ' . $comsort . ', name';
-		$result = mysql_query2($query);
-	    while ($row=fetchSqlAssoc($result))
-		{
-			$tablerows .= '<tr>';
-			$i = 0;
-			$id = 0;
-			foreach($row as $r)
-			{				
-				if($i ==0)
-				{
-					$tablerows .= '<td><a href="index.php?do=listitems&category=' . $cat . '&item=' . $r .'">' . $r . '</a>' . '</td>';
-					$id = $r;
-				}
-				else if($i == 1 && checkaccess('items', 'edit'))
-				{
-					$tablerows .= '<td>' . $r . ' (<a href="index.php?do=edititem&item=' . $id .'">edit</a>)' . '</td>';
-				}
-				else
-				{
-					$tablerows .= '<td>' . $r . '</td>';
-				}
-				$i++;
-			}
-			$tablerows .= '</tr>';
-		}
-	}
-    echo '<table border="1" class="top">';
-    echo '<tr>'.$tableheads.'</tr>';
-	echo($tablerows . '</table>');
-  }else{
-    echo '<p class="error">You are not authorized to use these functions!</p>';
-  }
+    $category_id = (isset($_GET['category_id']) ? $_GET['category_id'] : '');
+    $sort_col = (isset($_GET['sort_col']) ? $_GET['sort_col'] : 'name');
+    $sort_dir = (isset($_GET['sort_dir']) ? $_GET['sort_dir'] : 'asc');
+    
+    $makeSortUrl = function($colName) use (&$sort_col, &$sort_dir, &$category_id, &$compare_type)
+    {
+        // we use htmlentities on the whole string, so not using &amp; here.
+        $base = 'index.php?do=compareitems&category_id='.$category_id;
+        if ($colName == $sort_col && $sort_dir == 'asc')
+        {
+            return htmlentities($base.'&sort_col='.$colName.'&sort_dir=desc');
+        }// basically, else.
+        return htmlentities($base.'&sort_col='.$colName.'&sort_dir=asc');
+    };
+    
+
+    $categories = PrepSelect('category');
+    echo '<form method="get" action="index.php">'."\n";
+    echo '<div><input type="hidden" name="do" value="compareitems" />'."\n";
+    echo DrawSelectBox('category', $categories, 'category_id', $category_id);
+    echo '<input type="hidden" name="sort_col" value="'.htmlentities($sort_col).'" />'."\n";
+    echo '<input type="hidden" name="sort_dir" value="'.htmlentities($sort_dir).'" />'."\n";
+    echo '<input type="submit" name="View" value="View" /></div></form>'."\n";
+    
+    if ($category_id == '')
+    {   // no category_id, we're done.
+        return;
+    }
+    
+    $category_id = escapeSqlString($category_id);
+    $sort_col = escapeSqlString($sort_col);
+    $sort_dir = escapeSqlString($sort_dir);
+    
+    $query = "SELECT i.id, i.name, i.size, i.weight, i.base_sale_price, CONCAT(i.container_max_size, '-', i.container_max_slots) AS container";
+    $query .= ", i.item_max_quality, i.valid_slots, i.flags, i.description, i.spawnable, CONCAT_WS(', ', s1.name,s2.name,s3.name) AS item_skills";
+    $query .= ", i.consume_script, i.equip_script, CONCAT(i.item_bonus_1_attr, '=', i.item_bonus_1_max) AS item_bonus_1";
+    $query .= ", CONCAT(i.item_bonus_2_attr, '=', i.item_bonus_2_max) AS item_bonus_2, CONCAT(i.item_bonus_3_attr, '=', i.item_bonus_3_max) AS item_bonus_3";
+    $query .= ", CONCAT(i.requirement_1_name,'=',i.requirement_1_value) AS requirement1, CONCAT(i.requirement_2_name, '=',i.requirement_2_value) AS requirement2";
+    $query .= ", CONCAT(i.requirement_3_name,'=',i.requirement_3_value) AS requirement3, i.cstr_gfx_icon, i.cstr_gfx_mesh, i.cstr_gfx_texture";
+    $query .= ", i.cstr_part, i.cstr_part_mesh, i.removed_mesh, i.armor_hardness, i.armorvsweapon_type, i.weapon_block_targeted, i.weapon_block_untargeted";
+    $query .= ", i.weapon_counterblock, i.dmg_blunt, i.dmg_pierce, i.dmg_slash, i2.name as ammo, i.weapon_penetration, i.weapon_range, i.weapon_speed";
+    $query .= " FROM item_stats i LEFT JOIN skills AS s1 ON i.item_skill_id_1=s1.skill_id LEFT JOIN skills AS s2 ON i.item_skill_id_2=s2.skill_id";
+    $query .= " LEFT JOIN skills AS s3 ON i.item_skill_id_3=s3.skill_id LEFT JOIN item_stats AS i2 ON i.item_type_id_ammo=i2.id";
+    $query .= " WHERE i.category_id='$category_id' ORDER BY $sort_col $sort_dir, name";
+    
+    $result = mysql_query2($query);
+    if (sqlNumRows($result) == 0)
+    {
+        echo '<p class="error">There were no items in this category.</p>';
+        return;
+    }
+    
+    echo '<div id="scrollingtablediv" class="scrollingtablediv">'."\n";
+    echo '<table id="scrollingtable">'."\n";
+    echo '<thead class="scrollingthead">'."\n";
+    
+    $alt = false;
+    $count = 0;
+    echo '<tr class="color_'.(($alt = !$alt) ? 'a' : 'b').'">';
+    foreach (fetchSqlAssoc($result) as $colName => $colValue)
+    {
+        // first 2 are "frozen" columns
+        $thClass = '';
+        if ($count == 0)
+        {
+            $thClass = ' class="firstfrozencol color_'.($alt ? 'a' : 'b').'"';
+        }
+        else if ($count == 1)
+        {
+            $thClass = ' class="secondfrozencol color_'.($alt ? 'a' : 'b').'"';
+        }
+        echo '<th'.$thClass.'><a href="'.$makeSortUrl($colName).'">'.htmlentities($colName).'</a></th>';
+        $count++;
+    }
+    echo '</tr>'."\n";
+    echo '</thead>'."\n";
+    echo '<tbody class="scrollingtbody">'."\n";
+
+    // reset the result after our header printing.
+    sqlSeek($result, 0);
+    while ($row = fetchSqlAssoc($result))
+    {
+        echo '<tr class="color_'.(($alt = !$alt) ? 'a' : 'b').'">';
+        $count = 0;
+        $itemId = 0;
+        foreach ($row as $colValue)
+        {
+            $tdClass = '';
+            // first 2 are "frozen" columns, also contain view item and edit item links. The other columns are generated.
+            if ($count == 0)
+            {
+                $itemId = $colValue;
+                echo '<td class="firstfrozencol color_'.($alt ? 'a' : 'b').'"><a href="index.php?do=listitems&amp;category='.$category_id.'&amp;item='.$itemId.'">'.htmlentities($colValue).'</a></td>';
+            }
+            else if ($count == 1)
+            {
+                echo '<td class="secondfrozencol color_'.($alt ? 'a' : 'b').'">'.htmlentities($colValue);
+                if (checkaccess('items', 'edit'))
+                {
+                    echo '<a href="index.php?do=edititem&amp;item='.$itemId.'"> edit</a>';
+                }
+                echo '</td>';
+            }
+            else
+            {
+                echo '<td'.$tdClass.'>'.htmlentities($colValue).'</td>';
+            }
+            $count++;
+        }
+        echo '</tr>'."\n";
+    }
+    echo '</tbody>'."\n";
+    echo '</table>'."\n";
+    echo '</div>'."\n";
+    
+    // the following javascript ensures we have frozen columns, and makes all columns the proper width.
+    echo '
+        <script type="text/javascript">
+        //<![CDATA[
+        function myscroller() 
+        {
+            var left = document.getElementById("scrollingtablediv").scrollLeft;
+            var firsts = document.getElementsByClassName("firstfrozencol");
+            var seconds = document.getElementsByClassName("secondfrozencol");
+            for (var i = 0; i < firsts.length; i++) 
+            {
+                first = firsts.item(i);
+                first.style.left = left -(left > 6 ? 6 : 0) + "px"; 
+                
+                // only set a second frozen table if one is given.
+                if (seconds.length != 0)
+                { 
+                    seconds.item(i).style.borderRight = "1px solid black";
+                    // -2 to remove the "gap" between the 2 table elements. It is probably not technically correct, but since there is already an element 
+                    // at that position, it moves just to the right of it.
+                    seconds.item(i).style.left = parseFloat(first.style.left) - 2 + "px";
+                }
+                else
+                {
+                    first.style.borderRight = "1px solid black";
+                }
+            }
+        }
+        // add the above function to the table div as an event listener for scrolling (div scrolls horizontal, which is what we need).
+        document.getElementById("scrollingtablediv").addEventListener("scroll", myscroller);
+        
+        // number of th tags and amount of columns are considered to be equal.
+        var thElements = document.getElementsByClassName("scrollingthead").item(0).getElementsByTagName("tr").item(0).getElementsByTagName("th");
+        var tdElements = document.getElementsByClassName("scrollingtbody").item(0).getElementsByTagName("tr").item(0).getElementsByTagName("td");
+        var thIncrease = 0;
+        var tdIncrease = 0;
+        
+        // number of th tags and amount of columns are considered to be equal.
+        for (var i = 0; i < thElements.length; i++) 
+        {
+            var tdWidth = tdElements.item(i).getBoundingClientRect().width;
+            var thWidth = thElements.item(i).getBoundingClientRect().width;
+            var maxWidth = (thWidth < tdWidth ? tdWidth : thWidth);
+            // keep track of which row we increased in size by how much
+            thWidth < tdWidth ? thIncrease += tdWidth - thWidth : tdIncrease += thWidth - tdWidth;
+            thElements.item(i).style.width = maxWidth + "px";
+            tdElements.item(i).style.width = maxWidth + "px";
+        } 
+        
+        var cs = window.getComputedStyle;
+        // this script assumes matching left and right padding/margin/border width. In addition, it assumes equality between TH and TD tags in this respect.
+        var tdi = tdElements.item(0);
+        // we add + 1px to the table for each element. This is from unknown origin, but is required.
+        var additionalWidth =  thElements.length * (parseFloat(cs(tdi).getPropertyValue("border-left-width")) * 2 + 
+            parseFloat(cs(tdi).getPropertyValue("margin-left")) * 2 + parseFloat(cs(tdi).getPropertyValue("padding-left")) * 2 + 1);
+        // table should be increased by the smallest of td and th increase, + additional width for padding/margin/etc + 1 for IE.
+        var tableIncrease = (thIncrease < tdIncrease ? thIncrease : tdIncrease) + additionalWidth + 1; 
+        
+        var tableWidth = document.getElementById("scrollingtable").offsetWidth;
+        // set table to the proper width to match the new element sizes.
+        document.getElementById("scrollingtable").style.width = tableWidth + tableIncrease + "px";
+        //]]>
+        </script>'; // end of echo.
 }
 ?>
