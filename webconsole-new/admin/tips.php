22d9a30a55b5851609c5614e19123688d7887992
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-02-23 16:03:39 +0000

    Added an ID collumn to the table. You can now specify an ID while creating a tip (ID < 1000). Split the table in two, 1 for tips (ID < 1000) and 1 for tutorial tips. Fixed XHTML validity. Escaped contents of the tips for display. Changed password confirmation to just regular confirmation on delete tips.

--- webconsole-new/admin/tips.php
+++ webconsole-new/admin/tips.php
@@ -1,50 +1,60 @@
 <?PHP
-function listtips(){
-
-	if (checkaccess('admin', 'read')) 
-    {
-        $query = 'SELECT * FROM tips ORDER BY tip';
-        $result = mysql_query2($query);
-        echo'<table border="1">';
-        while ($row = fetchSqlAssoc($result))
-        {
-            echo '<tr><td>';
-            if(checkaccess('admin', 'edit')) 
-            {
-                echo '<form action="./index.php?do=edittips" method="post">';
-                echo '<textarea cols="50" rows="2" name="tip">'.$row['tip'].'</textarea>';
-                echo '<input type="hidden"  name="id" value ="'.$row['id'].'"/>';
-                echo '</td><td>';
-                echo '<input type="submit" name="action" value="Save"/>';
-                if(checkaccess('admin', 'delete')) 
-                {
-                    echo '<br/><input type="submit" name="action" value="Delete"/>';
-                }
-                echo '</form>';
-            }
-            else 
-            {
-                echo $row['tip'].'</td><td>';
-            }
-            echo '</td></tr>';
-        }
-        if(checkaccess('admin', 'create')) 
-        {
-            echo '<tr><td>';
-            echo '<form action="./index.php?do=edittips" method="post">';
-            echo '<textarea cols="50" rows="2" name="tip"></textarea>';
-            echo '</td><td>';
-            echo '<input type="submit" name="action" value="Add"/>';
-//            echo '</td></tr>';
-            echo '</form>';
-        }
-        echo '</td></tr>';
-        echo '</table>';
-	}
-    else
+function listtips()
+{
+	if (!checkaccess('admin', 'read')) 
     {
         echo '<p class="error">You are not authorized to use these functions</p>';
+        return;
     }
+    $query = 'SELECT id, tip FROM tips WHERE id < 1000 ORDER BY id';
+    $result = mysql_query2($query);
+    echo '<h3>Listing Tips</h3>';
+    printTable($result);
+    
+    $query = 'SELECT id, tip FROM tips WHERE id >= 1000 ORDER BY id';
+    $result = mysql_query2($query);
+    echo '<h3>Listing Tutorial Tips</h3>';
+    printTable($result);
+
+    if(checkaccess('admin', 'create')) 
+    {
+        echo '<h3>Add a new enty </h3>'."\n";
+        echo '<div class="table">'."\n";
+        echo '<div class="tr"><div class="td">ID (leave empty for auto)</div><div class="td">Tip</div><div class="td"></div></div>'."\n";
+        echo '<form action="./index.php?do=edittips" method="post" class="tr"><div class="td"><input type="text" name="id"/></div><div class="td">'."\n";
+        echo '<textarea cols="50" rows="2" name="tip"></textarea>'."\n";
+        echo '</div><div class="td">'."\n";
+        echo '<input type="submit" name="action" value="Add"/>'."\n";
+        echo '</div></form>'."\n"; // end tr
+        echo '</div>'."\n"; // end table
+    }
+}
+
+// internal support function, expects 1 result set containing "ID" and "tip" fields.
+function printTable($result)
+{
+    echo '<div class="table">';
+    while ($row = fetchSqlAssoc($result))
+    {
+        echo '<form action="./index.php?do=edittips" method="post" class="tr"><div class="td">'.$row['id'].'</div><div class="td">'."\n";
+        if(checkaccess('admin', 'edit')) 
+        {
+            echo '<textarea cols="50" rows="4" name="tip">'.htmlentities($row['tip']).'</textarea>'."\n";
+            echo '<input type="hidden"  name="id" value ="'.$row['id'].'" />'."\n";
+            echo '</div><div class="td">'."\n";
+            echo '<input type="submit" name="action" value="Save" />'."\n";
+            if(checkaccess('admin', 'delete')) 
+            {
+                echo '<br/><input type="submit" name="action" value="Delete" />'."\n";
+            }
+        }
+        else 
+        {
+            echo $row['tip'].'</div><div class="td">'."\n";
+        }
+        echo '</div></form>'."\n"; // end tr
+    }
+    echo '</div>'."\n"; // end table
 }
 
 function edittips(){
@@ -74,8 +84,15 @@ function edittips(){
 	if ($action == 'Add'){
 		if (checkaccess('admin', 'create'))
 		{
-            mysql_query2("INSERT INTO tips (tip) VALUES ('$tip')");
-            echo '<p class="error">Tip was successfully added.</p>';
+            if ($id != '' && is_numeric($id))
+            {
+                mysql_query2("INSERT INTO tips (id, tip) VALUES ('$id', '$tip')");
+            }
+            else
+            {
+                mysql_query2("INSERT INTO tips (tip) VALUES ('$tip')");
+            }
+            echo '<p class="error">Tip was successfully added.</p>'."\n";
             unset($_POST);
             listtips();
 		}
@@ -85,7 +102,7 @@ function edittips(){
 		if (checkaccess('admin', 'edit'))
         {
             mysql_query2("UPDATE tips SET tip = '$tip' WHERE id = '$id'");
-            echo '<p class="error">Tip was successfully edited.</p>';
+            echo '<p class="error">Tip was successfully edited.</p>'."\n";
             unset($_POST);
             listtips();
         }
@@ -93,31 +110,24 @@ function edittips(){
 	}
     elseif ($action == 'Delete' && isset($_POST['submit']) && $_POST['submit'] == 'Confirm Delete')
     {
-        $password = escapeSqlString($_POST['passd']);
-        $username = escapeSqlString($_SESSION['username']);
-        $query = "SELECT COUNT(username) FROM accounts WHERE username='$username' AND password=MD5('$password')";
-        $result = mysql_query2($query);
-        $row = fetchSqlRow($result);
-        if ($row[0] == 1)
+        if (checkaccess('admin', 'delete'))
         {
             mysql_query2("DELETE FROM tips WHERE id ='$id' LIMIT 1");
-            echo '<p class="error">Tip was successfully deleted.</p>';
+            echo '<p class="error">Tip was successfully deleted.</p>'."\n";
             unset($_POST);
             listtips();
-        }
-        else
-        {
-            echo '<p class="error">Password check failed - Did Not Delete Tip</p>';
         }
     }
 	elseif ($action == 'Delete')
     {
         if (checkaccess('admin', 'delete'))
         {
-            echo '<p class="error">Warning, you are about to permanently delete the following Tip:</p>';
-            echo '<p>'.$tip.'</p>';
-            echo '<form action="./index.php?do=edittips" method="post"><input type="hidden" name="action" value="Delete"/><input type="hidden" name="id" value="'.$id.'"/>';
-            echo 'Enter your password to confirm deletion the Tip listed above: <input type="password" name="passd"/><input type="submit" name="submit" value="Confirm Delete"/></form>';
+            echo '<p class="error">Warning, you are about to permanently delete the following Tip:</p>'."\n";
+            echo '<p>'.htmlentities($tip).'</p>'."\n";
+            echo '<p>Are you sure?</p>'."\n";
+            echo '<form action="./index.php?do=edittips" method="post"><div><input type="hidden" name="action" value="Delete"/>'."\n";
+            echo '<input type="hidden" name="id" value="'.$id.'"/>'."\n";
+            echo '<input type="submit" name="submit" value="Confirm Delete"/></div></form>'."\n";
 		}
 	}
 	else
