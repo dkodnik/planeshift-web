cba5cf996bcae16c21ef4ccb5600a451b681ff08
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-02-23 15:46:30 +0000

    added line-breaks for improved XHTML code readability.

--- webconsole-new/admin/adminmain.php
+++ webconsole-new/admin/adminmain.php
@@ -3,12 +3,12 @@ function adminmain()
 {
     if (checkaccess('admin', 'read'))
     {
-        echo '<div class="menu_left">';
-        echo '<a href="./index.php?do=listtips">List Tips</a><br/>';
-        echo '<a href="./index.php?do=viewcommands">View Commands</a><br/>'; 
-        echo '<a href="./index.php?do=viewserveroptions">View Server Options</a><br/>'; 
-        echo '<a href="./index.php?do=listgms">List GMs</a><br/>';
-        echo '<hr/><a href="./index.php">Return to main page.</a>';
+        echo '<div class="menu_left">'."\n";
+        echo '<a href="./index.php?do=listtips">List Tips</a><br/>'."\n";
+        echo '<a href="./index.php?do=viewcommands">View Commands</a><br/>'."\n"; 
+        echo '<a href="./index.php?do=viewserveroptions">View Server Options</a><br/>'."\n"; 
+        echo '<a href="./index.php?do=listgms">List GMs</a><br/>'."\n";
+        echo '<hr/><a href="./index.php">Return to main page.</a>'."\n";
         echo '</div><div class="main">';
   }
   else
