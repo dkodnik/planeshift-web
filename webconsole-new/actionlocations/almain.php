d14abc355cb37b1d21b9686c6f54345854455c64
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-02-29 17:06:25 +0000

    fixed XHTML validity and readability.

--- webconsole-new/actionlocations/almain.php
+++ webconsole-new/actionlocations/almain.php
@@ -1,16 +1,20 @@
 <?php
-function almain(){
-  if (checkaccess('als', 'read')){
-    echo '<div class="menu_left">';
-    echo '<a href="./index.php?do=alsector">Action Locations</a> <br/>';
-    echo '<a href="./index.php?do=listals&gameboards=yes">AL with Game boards</a> <br/>';
-    echo '<a href="./index.php?do=gameboards">Game boards</a> <br/>';
-    echo '<a href="./index.php?do=checkbooks">Check Books (* !!)</a> <br/>';
-    echo '(* !!) This query is made at runtime and can take lot of server resources!<br/>';
-    echo '<a href="./index.php">Return to main page</a>';
-    echo '</div><div class="main">'."\n";
-  }else{
-    echo '<p class="error">You are not authorized to use these functions</p>';
-  }
+function almain()
+{
+    if (checkaccess('als', 'read'))
+    {
+        echo '<div class="menu_left">';
+        echo '<a href="./index.php?do=alsector">Action Locations</a> <br/>'."\n";
+        echo '<a href="./index.php?do=listals&amp;gameboards=yes">AL with Game boards</a> <br/>'."\n";
+        echo '<a href="./index.php?do=gameboards">Game boards</a> <br/>'."\n";
+        echo '<a href="./index.php?do=checkbooks">Check Books (* !!)</a> <br/>'."\n";
+        echo '(* !!) This query is made at runtime and can take lot of server resources!<br/>'."\n";
+        echo '<a href="./index.php">Return to main page</a>';
+        echo '</div><div class="main">'."\n";
+    }
+    else
+    {
+        echo '<p class="error">You are not authorized to use these functions</p>';
+    }
 }
 ?>
