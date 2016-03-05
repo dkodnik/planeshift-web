7486b1434a6ef4dfc7730b9c29118b085c70b8b1
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-03-05 23:17:51 +0000

    fixed: list combat view no longer reports double weapons and skills where there are none. The list now abides the "X per page" option. Weapons and skills from master ID are now shown (if master is set). fixed: now makes valid XHTML.

--- www/webconsole-new/npcs/listnpcscombat.php
+++ www/webconsole-new/npcs/listnpcscombat.php
@@ -1,70 +1,31 @@
 <?php
 
-// This function isn't used until the save form below is done.
-//function SelectWeapon($current, $name, $weaponlist)
-//{
-//    $html = '<select name="'.$name.'">';
-//    $html.= '<option value="-1">None</option>';
-//    foreach($weaponlist as $row)
-//    {
-//        $selected = '';
-//        if($row['id'] == $current)
-//        {
-//            $selected = ' selected="selected"';
-//        }
-//        $html .= '<option value="'.$row['id'].'"'.$selected.'>['.htmlentities($row['armorvsweapon_type']).'] '.htmlentities($row['name']).'</option>';
-//    }
-//    return $html.'</select>';
-//}
-
 function listnpcscombat()
 {
     if (checkaccess('npcs', 'read'))
     {
-        $sql = 'SELECT c.id, c.name, c.lastname, c.npc_spawn_rule, c.npc_addl_loot_category_id, c.kill_exp, s.name AS sector, c.loc_x, c.loc_y, c.loc_z, c.loc_instance, b.region, i.location_in_parent AS weapon_location, i2.id AS weapon_id, i2.name AS weapon_name, cs.skill_id, sk.name AS skill_name, cs.skill_rank FROM characters as c LEFT JOIN sectors AS s ON c.loc_sector_id=s.id LEFT JOIN sc_npc_definitions AS b ON c.id=b.char_id LEFT JOIN item_instances AS i ON i.char_id_owner=c.id AND (i.location_in_parent = 0 OR i.location_in_parent = 1) LEFT JOIN item_stats AS i2 ON i.item_stats_id_standard = i2.id LEFT JOIN character_skills AS cs ON cs.character_id = c.id LEFT JOIN skills AS sk ON sk.skill_id = cs.skill_id WHERE c.character_type = 1';
-        $sql.= ' AND c.npc_impervious_ind=\'N\'';
-
-        $sql2 = "SELECT COUNT(*) FROM characters WHERE character_type = 1 AND npc_impervious_ind='N'";
-        $item_count = fetchSqlRow(mysql_query2($sql2));
+        $sql = "SELECT c.id, c.name, c.lastname, c.npc_spawn_rule, c.npc_addl_loot_category_id, c.kill_exp, c.npc_master_id, s.name AS sector, c.loc_x, ";
+        $sql .= "c.loc_y, c.loc_z, c.loc_instance, sc_npc_d.region FROM characters as c LEFT JOIN sectors AS s ON c.loc_sector_id = s.id ";
+        $sql .= "LEFT JOIN sc_npc_definitions AS sc_npc_d ON c.id = sc_npc_d.char_id WHERE c.character_type = 1 AND c.npc_impervious_ind='N'";
+        //$sql = 'SELECT c.id, c.name, c.lastname, c.npc_spawn_rule, c.npc_addl_loot_category_id, c.kill_exp, s.name AS sector, c.loc_x, c.loc_y, c.loc_z, c.loc_instance, b.region, i.location_in_parent AS weapon_location, i2.id AS weapon_id, i2.name AS weapon_name, cs.skill_id, sk.name AS skill_name, cs.skill_rank FROM characters as c LEFT JOIN sectors AS s ON c.loc_sector_id=s.id LEFT JOIN sc_npc_definitions AS b ON c.id=b.char_id LEFT JOIN item_instances AS i ON i.char_id_owner=c.id AND (i.location_in_parent = 0 OR i.location_in_parent = 1) LEFT JOIN item_stats AS i2 ON i.item_stats_id_standard = i2.id LEFT JOIN character_skills AS cs ON cs.character_id = c.id LEFT JOIN skills AS sk ON sk.skill_id = cs.skill_id WHERE c.character_type = 1';
         
-        if (isset($_GET['sort']))
+        $sorting = array('id' => 'c.id', 'name' => 'c.name', 'sector' => 's.name, c.name', 'spawn' => 'c.npc_spawn_rule, c.name', 'loot' => 'c.npc_addl_loot_category_id, c.name');
+        if (isset($_GET['sort']) && array_key_exists($_GET['sort'], $sorting))
         {
-            if ($_GET['sort'] == 'id')
-            {
-                $sql = $sql . ' ORDER BY c.id';
-            }
-            else if ($_GET['sort'] == 'name')
-            {
-                $sql = $sql . ' ORDER BY c.name';
-            }
-            else if ($_GET['sort'] == 'sector')
-            {
-                $sql = $sql . ' ORDER BY s.name, c.name';
-            }
-            else if ($_GET['sort'] == 'spawn')
-            {
-                $sql = $sql . ' ORDER BY c.npc_spawn_rule, c.name';
-            }
-            else if ($_GET['sort'] == 'loot')
-            {
-                $sql = $sql . ' ORDER BY c.npc_addl_loot_category_id, c.name';
-            }
-            else
-            {
-                $sql = $sql . ' ORDER BY s.name, c.name';
-            }
+            $sql .= ' ORDER BY '.$sorting[$_GET['sort']];
         }
         else
         {
-            $sql = $sql . ' ORDER BY sector, name';
+            $sql .= ' ORDER BY sector, name';
         }
 
+        $sql2 = "SELECT COUNT(*) FROM characters WHERE character_type = 1 AND npc_impervious_ind='N'";
+        $item_count = fetchSqlRow(mysql_query2($sql2));
         $nav = RenderNav('do=listnpcscombat', $item_count[0]);
         $sql .= $nav['sql'];
         echo $nav['html'];
         unset($nav);
         
-
         $query = mysql_query2($sql);
         if (sqlNumRows($query) == 0)
         {
@@ -73,61 +34,15 @@ function listnpcscombat()
         }
 
         echo '<table border="1">';
-        echo '<tr><th><a href="./index.php?do=listnpcscombat&amp;sort=id">ID</a></th><th><a href="./index.php?do=listnpcscombat&amp;sort=name">Name</a></th><th><a href="./index.php?do=listnpcscombat&amp;sort=spawn">Spawn</a>/<a href="./index.php?do=listnpcscombat&amp;sort=loot">Loot</a></th><th>Position</th><th><a href="./index.php?do=listnpcscombat&amp;sort=sector">Sector</a></th><th>Region</th><th>Weapon Right Hand</th><th>Weapon Left Hand</th><th>Skills</th><th>Exp</th><th> </th></tr>';
+        echo '<tr><th><a href="./index.php?do=listnpcscombat&amp;sort=id">ID</a></th><th><a href="./index.php?do=listnpcscombat&amp;sort=name">Name</a></th><th><a href="./index.php?do=listnpcscombat&amp;sort=spawn">Spawn</a>/<a href="./index.php?do=listnpcscombat&amp;sort=loot">Loot</a></th><th>Loc X/Y/Z/Instance</th><th><a href="./index.php?do=listnpcscombat&amp;sort=sector">Sector</a></th><th>Region</th><th>Weapon Right Hand</th><th>Weapon Left Hand</th><th>Skills</th><th>Exp</th></tr>';
 
-        $npcs = array();
+        // cs.skill_id, sk.name AS skill_name, cs.skill_rank FROM character_skills AS cs ON cs.character_id = c.id LEFT JOIN skills AS sk ON sk.skill_id = cs.skill_id
         while ($row = fetchSqlAssoc($query))
         {
-            // this if/else will fire some times for each specific npc to parse all it's skills and weapons.
-            if(!isset($npcs[$row['id']]))
-            {
-                // Assigning additional variables to $row before adding them to npcs ($row will get overwritten each iteration)
-                $row['weapons'] = array($row['weapon_location'] => array($row['weapon_id'], $row['weapon_name']));
-                $row['skills'] = array();
-                if($row['skill_id'] != null)
-                {
-                    $row['skills'][$row['skill_id']] = array($row['skill_name'], $row['skill_rank']);
-                }
-                unset($row['weapon_id'], $row['weapon_location'], $row['skill_id'], $row['skill_name'], $row['skill_rank']);
+            // get skills and weapons from master NPC if set, otherwise get own. if master is set to this npcs own ID (no master) this works too.
+            $id = ($row['npc_master_id'] != 0 ? $row['npc_master_id'] : $row['id']);
 
-                $npcs[$row['id']] = $row;
-            }
-            else
-            {
-                // Check for errors before adding the weapons / skills to the npc list. 
-                $w = $npcs[$row['id']]['weapons'];
-                if(count($w) > 1)
-                {
-                    echo '<p class="error">Found more than two weapon for NPC #'.$row['id'].'!</p>';
-                }
-                elseif(isset($w[$row['weapon_location']]))
-                {
-                    echo '<p class="error">Found more than one weapon in the same location for NPC #'.$row['id'].'!</p>';
-                }
-                else
-                {
-                    $w[$row['weapon_location']] = array($row['weapon_id'], $row['weapon_name']);
-                    $npcs[$row['id']]['weapons'] = $w;
-                }
-
-                $s = $npcs[$row['id']]['skills'];
-                if(isset($s[$row['skill_id']]))
-                {
-                    echo '<p class="error">Found the same skill two times for NPC #'.$row['id'].'!</p>';
-                }
-                else
-                {
-                    $s[$row['skill_id']] = array($row['skill_name'], $row['skill_rank']);
-                    $npcs[$row['id']]['skills'] = $s;
-                }
-            }
-        }
-        foreach($npcs as $row)
-        {
-            $right = (isset($row['weapons'][0]) ? $row['weapons'][0] : array(-1, 'None'));
-            $left = (isset($row['weapons'][1]) ? $row['weapons'][1] : array(-1, 'None'));
-
-            echo '<form action="./index.php?do=npc_details&amp;sub=main&amp;npc_id='.$row['id'].'" method="post"><tr style="white-space: nowrap;">';
+            echo '<tr style="white-space: nowrap;">';
             echo '<td>'.$row['id'].'</td>';
             if (checkaccess('npcs', 'edit'))
             {
@@ -142,21 +57,38 @@ function listnpcscombat()
             echo '<td>'.$row['loc_x'].' / '.$row['loc_y'].' / '.$row['loc_z'].' / '.$row['loc_instance'].'</td>';
             echo '<td>'.$row['sector'].'</td>';
             echo '<td>'.$row['region'].'</td>';
-            //echo '<td>'.SelectWeapon($right[0], 'righthand', $weaponlist).'</td>';
-            //echo '<td>'.SelectWeapon($left[0], 'lefthand', $weaponlist).'</td>';
-            echo '<td>'.htmlentities($right[1]).'</td>';
-            echo '<td>'.htmlentities($left[1]).'</td>';
-            echo '<td>';
-            foreach($row['skills'] as $skill)
+            $right = '';
+            $left = '';
+            $sql2 = "SELECT i.location_in_parent AS weapon_location, ist.name AS weapon_name FROM item_instances AS i LEFT JOIN item_stats AS ist ";
+            $sql2 .= "ON i.item_stats_id_standard = ist.id WHERE i.char_id_owner = '$id' AND (i.location_in_parent = 0 OR i.location_in_parent = 1)";
+            $query2 = mysql_query2($sql2);
+            if (sqlNumRows($query2) > 2)
             {
-                echo htmlentities($skill[0]).': '.$skill[1].' ';
+                echo '<p class="error">NPC #$id has too many weapons.</p>';
+            } // we expect 2 results max.
+            while ($row2 = fetchSqlAssoc($query2))
+            {
+                if ($row2['weapon_location'] == 0)
+                {
+                    $right = $row2['weapon_name'];
+                }
+                else 
+                {
+                    $left = $row2['weapon_name'];
+                }
+            }
+            echo '<td>'.htmlentities($right).'</td>';
+            echo '<td>'.htmlentities($left).'</td>';
+            echo '<td>';
+            $sql2 = "SELECT sk.name AS skill_name, cs.skill_rank FROM character_skills AS cs LEFT JOIN skills AS sk ON sk.skill_id = cs.skill_id WHERE cs.character_id = '$id'";
+            $query2 = mysql_query2($sql2);
+            while ($row2 = fetchSqlAssoc($query2))
+            {
+                echo htmlentities($row2['skill_name']).': '.$row2['skill_rank'].' ';
             }
             echo '</td>';
-            //echo '<td><input type="text" name="kill_exp" value="'.$row['kill_exp'].'" size="2" /></td>';
             echo '<td>'.$row['kill_exp'].'</td>';
-            //echo '<td><input type="submit" name="commit" value="Save" /></td>';
-            echo '<td></td>';
-            echo '</tr></form>';
+            echo '</tr>';
         }
         echo '</table>';
     }
