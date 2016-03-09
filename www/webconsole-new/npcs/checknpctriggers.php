a1f52dfa9820c55c997af6a1463edca3ab7ce437
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-03-09 16:15:16 +0000

    check npc triggers now also finds stacked triggers like "howdy. greetings. hello." (3 triggers in one). Revised some of the code, this script should now need 50% less SQL queries.

--- www/webconsole-new/npcs/checknpctriggers.php
+++ www/webconsole-new/npcs/checknpctriggers.php
@@ -1,77 +1,86 @@
-<?PHP
-function checknpctriggers(){
+<?php
+function checknpctriggers()
+{
 
-    if (checkaccess('npcs', 'read'))
-    {
-        // extract all invulnerable NPC names
-        $query = "select c.id, c.name, c.lastname, sec.name, c.loc_x, c.loc_y, c.loc_z, c.npc_spawn_rule from characters as c, sectors as sec ";
-        $query = $query . " where c.npc_master_id !=0 and c.loc_sector_id=sec.id";
-        $query = $query . " and c.npc_impervious_ind='Y'";
-        $query = $query . " order by sec.name, c.name";
-
-        $result = mysql_query2($query);
-
-        echo '<table border="1">';
-        echo '<tr><th> ID </th><th> Name</th><th> Sector</th><th> Position</th><th> Missing Triggers</th><th> Loaded in game?</th></tr>';
-
-        // for each NPC
-        while ($line = fetchSqlRow($result)) 
-        {
-            $fullname = $line[1] . " " . $line[2];
-            $fullname = trim($fullname);
-            // full name gets used below as well, so we can't just escape that one.
-            $fullNameEscaped = escapeSqlString($fullname);
-            // search his triggers
-            $query2 = "select count(*) from npc_triggers where area='$fullNameEscaped' and trigger_text='greetings'";
-            $result2 = mysql_query2($query2);
-            $line2 = fetchSqlRow($result2);
-            $found1 = $line2[0];
-            if ($found1=="0") {
-                $query2 = "select count(*) from npc_knowledge_areas where area like 'greetings%' and player_id=$line[0]";
-                $result2 = mysql_query2($query2);
-                $line2 = fetchSqlRow($result2);
-                $found1 = $line2[0];
-            }
-
-            $query2 = "select count(*) from npc_triggers where area='$fullNameEscaped' and trigger_text='about you'";
-            $result2 = mysql_query2($query2);
-            $line2 = fetchSqlRow($result2);
-            $found2 = $line2[0];
-
-            $query2 = "select count(*) from npc_triggers where area='$fullNameEscaped' and trigger_text='how you'";
-            $result2 = mysql_query2($query2);
-            $line2 = fetchSqlRow($result2);
-            $found3 = $line2[0];
-            
-            $loaded = "yes";
-            if ($line[7]==0)
-            {
-                $loaded = "<font color=red>no</font>";
-            }
-
-            if ($found1=="0" or $found2=="0" or $found3=="0") {
-                echo "<tr><td>$line[0] </td><td><a href=\"index.php?do=npc_details&amp;sub=main&amp;npc_id=".$line[0].'">'.$fullname."</a></td><td>$line[3]</td><td>$line[4]/$line[5]/$line[6]</td><td>";
-                if ($found1=="0")
-                {
-                    echo "greetings <br/>";
-                }
-                if ($found2=="0")
-                {
-                    echo "about you <br/>";
-                }
-                if ($found3=="0")
-                {
-                    echo "how you <br/>";
-                }
-                echo "</td><td>$loaded</td></tr>";
-            }
-        }
-        echo '</table><br/><br/>';
-    }
-    else
+    if (!checkaccess('npcs', 'read'))
     {
         echo '<p class="error">You are not authorized to use these functions</p>';
+        return;
     }
+    // extract all invulnerable NPC names
+    $sql = "SELECT c.id, c.name, c.lastname, s.name AS sector_name, c.loc_x, c.loc_y, c.loc_z, c.npc_spawn_rule FROM characters AS c LEFT JOIN sectors AS s ";
+    $sql .= " ON c.loc_sector_id = s.id WHERE c.npc_master_id !=0 AND c.npc_impervious_ind = 'Y' ORDER BY s.name, c.name";
+
+    $result = mysql_query2($sql);
+
+    echo '<table border="1">';
+    echo '<tr><th> ID </th><th>Name</th><th>Sector</th><th>Position</th><th> Missing Triggers</th><th> Loaded in game?</th></tr>'."\n";
+
+    // for each NPC
+    while ($row = fetchSqlAssoc($result)) 
+    {
+        $fullname = $row['name'].' '.$row['lastname'];
+        $fullname = trim($fullname);
+        // full name gets used below as well, so we can't just escape that one.
+        $fullNameEscaped = escapeSqlString($fullname);
+        
+        // search all triggers for this npc.
+        $foundGreetings = false;
+        $foundAboutYou = false;
+        $foundHowYou = false;
+        $sql2 = "SELECT trigger_text FROM npc_triggers WHERE area = '$fullNameEscaped'";
+        $result2 = mysql_query2($sql2);
+        while ($row2 = fetchSqlAssoc($result2))
+        {
+            // multiple triggers may be stacked seperated by dots.
+            $triggers = explode('.', $row2['trigger_text']);
+            foreach ($triggers as $trigger)
+            {
+                if (strtolower(trim($trigger)) == 'greetings')
+                {
+                    $foundGreetings = true;
+                }
+                if (strtolower(trim($trigger)) == 'about you')
+                {
+                    $foundAboutYou = true;
+                }
+                if (strtolower(trim($trigger)) == 'how you')
+                {
+                    $foundHowYou = true;
+                }
+            }
+        }
+        if (!$foundGreetings)
+        {
+            $sql2 = "select count(*) from npc_knowledge_areas where area like 'greetings%' and player_id = {$row['id']} ";
+            $result2 = mysql_query2($sql2);
+            $row2 = fetchSqlRow($result2);
+            if ($row2[0] > 0)
+            {
+                $foundGreetings = true;
+            }
+        }
+
+        if (!$foundGreetings || !$foundAboutYou || !$foundHowYou) 
+        {
+            echo '<tr><td>'.$row['id'].'</td><td><a href="index.php?do=npc_details&amp;sub=main&amp;npc_id='.$row['id'] .'">'.htmlentities($fullname).'</a></td>';
+            echo '<td>'.$row['sector_name'].'</td><td>'.$row['loc_x'].'/'.$row['loc_y'].'/'.$row['loc_z'].'</td><td>';
+            if (!$foundGreetings)
+            {
+                echo 'greetings <br/>';
+            }
+            if (!$foundAboutYou)
+            {
+                echo 'about you <br/>';
+            }
+            if (!$foundHowYou)
+            {
+                echo 'how you <br/>';
+            }
+            echo '</td><td>'.($row['npc_spawn_rule'] == 0 ? '<span class="error">no</span>' : 'yes').'</td></tr>'."\n";
+        }
+    }
+    echo '</table><br/><br/>';
 }
 
 ?>
