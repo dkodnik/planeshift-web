30ce485f99772efd547bcf1eef69a4b6a14f79b9
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-03-13 13:09:01 +0000

    Added a page to view, edit, create and delete Character Life Events.

--- www/webconsole-new/other/othermain.php
+++ www/webconsole-new/other/othermain.php
@@ -11,6 +11,7 @@ function othermain()
         echo '<a href="./index.php?do=listtraits">List traits</a> <br/>'; 
         echo '<a href="./index.php?do=showraces">Traits per race</a> <br/>'; 
         echo '<a href="./index.php?do=charactercreationevents">Character creation events</a> <br/>'; 
+        echo '<a href="./index.php?do=characterlifeevents">Character life events</a> <br/>'; 
         echo '<a href="./index.php?do=events">List GM Events</a><br/>';
         //echo '<a href="./index.php?do=listcommonstrings">List Common Strings</a> <br/>'; commented pending a decision on what to do with this since there is no more such table, but the information is still out there.
         
