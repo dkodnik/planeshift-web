802b0d94b0b0d4695f4eed9eb3882737a993a9f4
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-03-13 13:09:01 +0000

    Added a page to view, edit, create and delete Character Life Events.

--- webconsole-new/other/character_life_events.php
+++ webconsole-new/other/character_life_events.php
@@ -1,11 +1,11 @@
 <?php
 
-function charactercreationevents() 
+function characterlifeevents() 
 {
     // block unauthorized access
     if (isset($_POST['commit']) && !checkaccess('other', 'edit')) 
     {
-        echo '<p class="error">You are not authorized to edit Creation Events</p>';
+        echo '<p class="error">You are not authorized to edit Life Events</p>';
         return;
     }
     
@@ -16,15 +16,15 @@ function charactercreationevents()
         $description = escapeSqlString($_POST['description']);
         $cp_cost = escapeSqlString($_POST['cp_cost']);
         $scriptname = escapeSqlString($_POST['scriptname']);
-        $choice_area = escapeSqlString($_POST['choice_area']);
-        $sql = "INSERT INTO character_creation (name, description, cp_cost, scriptname, choice_area) VALUES ('$name', '$description', '$cp_cost', '$scriptname', '$choice_area')";
+        $is_base = escapeSqlString($_POST['is_base']);
+        $sql = "INSERT INTO char_create_life (name, description, cp_cost, scriptname, is_base) VALUES ('$name', '$description', '$cp_cost', '$scriptname', '$is_base')";
         mysql_query2($sql);
         echo '<p class="error">Event added.</p>';
     }
     elseif (isset($_POST['commit']) && $_POST['commit'] == 'Confirm Delete')
     {
         $id = escapeSqlString($_POST['id']);
-        $sql = "DELETE FROM character_creation WHERE id='$id'";
+        $sql = "DELETE FROM char_create_life WHERE id='$id'";
         mysql_query2($sql);
         echo '<p class="error">Delete succesfull</p>';
     }
@@ -35,23 +35,36 @@ function charactercreationevents()
         $description = escapeSqlString($_POST['description']);
         $cp_cost = escapeSqlString($_POST['cp_cost']);
         $scriptname = escapeSqlString($_POST['scriptname']);
-        $choice_area = escapeSqlString($_POST['choice_area']);
-        $sql = "UPDATE character_creation SET name='$name', description='$description', cp_cost='$cp_cost', scriptname='$scriptname', choice_area='$choice_area' WHERE id = '$id'";
+        $is_base = escapeSqlString($_POST['is_base']);
+        $sql = "UPDATE char_create_life SET name='$name', description='$description', cp_cost='$cp_cost', scriptname='$scriptname', is_base='$is_base' WHERE id = '$id'";
         mysql_query2($sql);
         echo '<p class="error">Update succesfull</p>';
     }
     
-    // if we print something for any of these actions, nothing else gets printed (no creation event list).
+    $baseList = array('Y', 'N');
+    $makeListDropdown = function ($name, $list, $selected = -1) 
+    {
+        $output = '';
+        $output .= '<select name="'.$name.'">';
+        foreach ($list as $value)
+        {
+            $output .= '<option value="'.$value.'" '.($value == $selected ? 'selected="selected"' : '').'>'.$value.'</option>';
+        }
+        $output .= '</select>';
+        return $output;
+    };
+    
+    // if we print something for any of these actions, nothing else gets printed (no life event list).
     if (isset($_GET['action']) && $_GET['action'] == 'edit')
     {
         // edit form
         $id = escapeSqlString($_GET['id']);
-        $sql = "SELECT name, description, cp_cost, scriptname, choice_area FROM character_creation WHERE id = '$id'";
+        $sql = "SELECT name, description, cp_cost, scriptname, is_base FROM char_create_life WHERE id = '$id'";
         $result = mysql_query2($sql);
         $row = fetchSqlAssoc($result);
     
         echo '<p>Editing Creation Event: </p>';
-        echo '<form action="./index.php?do=charactercreationevents" method="post">';
+        echo '<form action="./index.php?do=characterlifeevents" method="post">';
         echo '<table border="1">';
         echo '<tr><th>Field</th><th>Value</th></tr>';
         echo '<tr><td>ID</td><td><input type="hidden" name="id" value="'.$id.'" />'.$id.'</td></tr>';
@@ -60,7 +73,7 @@ function charactercreationevents()
         echo '<tr><td>CP Cost</td><td><input type="text" name="cp_cost" value="'.htmlentities($row['cp_cost']).'" /></td></tr>';
         $scripts = PrepSelect('scripts');
         echo '<tr><td>Script Name</td><td>'.DrawSelectBox('scripts', $scripts, 'scriptname', $row['scriptname']).'</td></tr>';
-        echo '<tr><td>Choice Area</td><td><input type="text" name="choice_area" value="'.htmlentities($row['choice_area']).'" /></td></tr>';
+        echo '<tr><td>Is Base</td><td>'.$makeListDropdown('is_base', $baseList, $row['is_base']).'</td></tr>';
         echo '<tr><td colspan="2"><input type="submit" name="commit" value="Save Changes" /></td></tr>';
         echo '</table>';
         echo '</form>';
@@ -70,26 +83,26 @@ function charactercreationevents()
     {
         // confirm delete
         $id = escapeSqlString($_GET['id']);
-        echo '<p class="error">You are about to delete Creation Event id "'.$id.'" </p>';
-        echo '<form action="./index.php?do=charactercreationevents" method="post">';
+        echo '<p class="error">You are about to delete Life Event id "'.$id.'" </p>';
+        echo '<form action="./index.php?do=characterlifeevents" method="post">';
         echo '<div><input type="hidden" name="id" value="'.$id.'" /><input type="submit" name="commit" value="Confirm Delete" /></div>';
         echo '</form>';
         return;
     }
     
     // Display the main list
-    $sql = "SELECT id, name, description, cp_cost, scriptname, choice_area FROM character_creation ORDER BY choice_area, name";
+    $sql = "SELECT id, name, description, cp_cost, scriptname, is_base FROM char_create_life ORDER BY name";
     $result = mysql_query2($sql);
     
     if (sqlNumRows($result) == 0)
     {
-        echo '<p class="error">No Character Creation Event data found.</p>';
+        echo '<p class="error">No Character Life Event data found.</p>';
     }
     else
     {
         // main list
         echo '<table border="1">'."\n";
-        echo '<tr><th>ID</th><th>Name</th><th>Description</th><th>CP Cost</th><th>Script Name</th><th>Choice Area</th><th>Actions</th></tr>'."\n";
+        echo '<tr><th>ID</th><th>Name</th><th>Description</th><th>CP Cost</th><th>Script Name</th><th>Is Base</th><th>Actions</th></tr>'."\n";
         
         while ($row = fetchSqlAssoc($result))
         {
@@ -99,11 +112,11 @@ function charactercreationevents()
             echo '<td>'.htmlentities($row['description']).'</td>';
             echo '<td>'.htmlentities($row['cp_cost']).'</td>';
             echo '<td>'.htmlentities($row['scriptname']).'</td>';
-            echo '<td>'.htmlentities($row['choice_area']).'</td>';
+            echo '<td>'.htmlentities($row['is_base']).'</td>';
             echo '<td>';
             if (checkAccess('other', 'edit'))
             {
-                $url = './index.php?do=charactercreationevents&amp;id='.$row['id'];
+                $url = './index.php?do=characterlifeevents&amp;id='.$row['id'];
                 echo '<a href="'.$url.'&amp;action=edit">Edit</a> - <a href="'.$url.'&amp;action=delete">Delete</a>';
             }
             echo '</td>';
@@ -115,7 +128,7 @@ function charactercreationevents()
     {
         // create form
         echo '<hr/><p>Create new Creation Event: </p>';
-        echo '<form action="./index.php?do=charactercreationevents" method="post">';
+        echo '<form action="./index.php?do=characterlifeevents" method="post">';
         echo '<table border="1">';
         echo '<tr><th>Field</th><th>Value</th></tr>';
         echo '<tr><td>Name</td><td><input type="text" name="name" /></td></tr>';
@@ -123,7 +136,7 @@ function charactercreationevents()
         echo '<tr><td>CP Cost</td><td><input type="text" name="cp_cost" /></td></tr>';
         $scripts = PrepSelect('scripts');
         echo '<tr><td>Script Name</td><td>'.DrawSelectBox('scripts', $scripts, 'scriptname', '').'</td></tr>';
-        echo '<tr><td>Choice Area</td><td><input type="text" name="choice_area" /></td></tr>';
+        echo '<tr><td>Is Base</td><td>'.$makeListDropdown('is_base', $baseList).'</td></tr>';
         echo '<tr><td colspan="2"><input type="submit" name="commit" value="Create Event" /></td></tr>';
         echo '</table>';
         echo '</form>';
