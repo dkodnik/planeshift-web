1d32c7c500972c7c49bdf8620dc33c1a7f7eddba
ravna-ps <ravna-ps@2752fbe2-5038-0410-9d0a-88578062bcef>
2016-12-29 01:11:30 +0000

    Added the $pathToWC variable

--- www/secure/db_config.php
+++ www/secure/db_config.php
@@ -3,4 +3,5 @@
     $db_username = "planeshift";
     $db_password = "planeshift";
     $db_name = "planeshift";
+    $pathToWC = './../webconsole-new/'; // this *must* end in a /, use ./ if the script is in the WC root, otherwise use an additional ../ for every directory it is below that.
 ?>
